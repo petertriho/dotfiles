e014f98b549e08f05b7b4f31c3095ba1b720537a
Peter Ho <mail@petertriho.com>
2021-02-13 14:55:18 +1100

    fish: move visual related settings to set_theme, rearrange when variables are set

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,21 @@
-starship init fish | source
+set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
+
+test -e /usr/local/opt/asdf/asdf.fish; and \
+    source /usr/local/opt/asdf/asdf.fish
 
 set -gx EDITOR nvim
 set -gx PROJECT_PATHS $HOME/Documents/GitHub $HOME/Documents/Projects
+
+set -gx COMPOSE_DOCKER_CLI_BUILD 1
+
+set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
 
 if status is-interactive
     test -e {$HOME}/.iterm2_shell_integration.fish; and \
         source {$HOME}/.iterm2_shell_integration.fish
 
-    set -g fish_greeting
-    set -g fish_key_bindings fish_hybrid_key_bindings
-
-    set -g fish_cursor_default block
-    set -g fish_cursor_insert line
-    set -g fish_cursor_replace_one underscore
-    set -g fish_cursor_visual block
+    starship init fish | source
 
     set -gx FZF_DEFAULT_COMMAND \
        "rg" \
@@ -34,13 +36,3 @@ if status is-interactive
        "--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
     set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
 end
-
-test -e /usr/local/opt/asdf/asdf.fish; and \
-    source /usr/local/opt/asdf/asdf.fish
-
-set -gx COMPOSE_DOCKER_CLI_BUILD 1
-
-set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
-
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
--- fish/.config/fish/functions/set_theme.fish
+++ fish/.config/fish/functions/set_theme.fish
@@ -1,4 +1,12 @@
 function set_theme --description "Set onedark theme"
+    set -U fish_greeting
+    set -U fish_key_bindings fish_hybrid_key_bindings
+
+    set -U fish_cursor_default block
+    set -U fish_cursor_insert line
+    set -U fish_cursor_replace_one underscore
+    set -U fish_cursor_visual block
+
     set -Ux LS_COLORS (vivid generate one-dark)
     set_onedark
 end
