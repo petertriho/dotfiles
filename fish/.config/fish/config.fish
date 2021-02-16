17f0601a970cab2d889e23936684754e085c0bff
Peter Ho <mail@petertriho.com>
2021-02-16 21:36:01 +1100

    fish: include hidden files when using fd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -35,4 +35,5 @@ if status is-interactive
        "--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe" \
        "--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
     set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
+    set -g fzf_fd_opts --hidden --exclude=.git
 end
