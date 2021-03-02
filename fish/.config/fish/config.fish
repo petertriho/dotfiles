eec1da9d578a909e874a98ffbcb13756a3026c12
Peter Ho <mail@petertriho.com>
2021-03-03 09:34:00 +1100

    fish: change to using fish_add_path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
-set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
+fish_add_path $HOME/.local/bin
 
 test -e /usr/local/opt/asdf/asdf.fish; and \
-    source /usr/local/opt/asdf/asdf.fish
+   source /usr/local/opt/asdf/asdf.fish
 
 set -gx EDITOR nvim
 set -gx PROJECT_PATHS $HOME/Documents/GitHub $HOME/Documents/Projects
@@ -12,28 +12,28 @@ set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
 set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
 
 if status is-interactive
-    test -e {$HOME}/.iterm2_shell_integration.fish; and \
-        source {$HOME}/.iterm2_shell_integration.fish
+   test -e {$HOME}/.iterm2_shell_integration.fish; and \
+      source {$HOME}/.iterm2_shell_integration.fish
 
-    starship init fish | source
+   starship init fish | source
 
-    set -gx FZF_DEFAULT_COMMAND \
-       "rg" \
-       "--files" \
-       "--follow" \
-       "--no-ignore-vcs" \
-       "--hidden" \
-       "--glob '!{.git/*,.venv/*,__pycache__/*,node_modules/*,build/*,dist/*}'"
-    set -gx FZF_DEFAULT_OPTS \
-       "--exact" \
-       "--border" \
-       "--cycle"  \
-       "--reverse" \
-       "--height '80%'" \
-       "--bind 'alt-j:preview-down,alt-k:preview-up,alt-a:select-all,alt-d:deselect-all'" \
-       "--color=dark" \
-       "--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe" \
-       "--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
-    set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
-    set -g fzf_fd_opts --hidden --exclude=.git
+   set -gx FZF_DEFAULT_COMMAND \
+      "fd" \
+      "--type file" \
+      "--color always" \
+      "--follow" \
+      "--hidden" \
+      "--exclude .git"
+   set -gx FZF_DEFAULT_OPTS \
+      "--ansi" \
+      "--exact" \
+      "--border" \
+      "--cycle"  \
+      "--reverse" \
+      "--height '80%'" \
+      "--bind 'alt-j:preview-down,alt-k:preview-up,alt-a:select-all,alt-d:deselect-all'" \
+      "--color=dark" \
+      "--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe" \
+      "--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
+   set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
 end
