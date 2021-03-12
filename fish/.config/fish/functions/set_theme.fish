f5e45af6aa37ecd03389562d2f9bdfbe8dbb8b6d
Peter Ho <mail@petertriho.com>
2021-03-12 21:12:47 +1100

    fish: change to ansi colors, remove onedark plugin

--- fish/.config/fish/functions/set_theme.fish
+++ fish/.config/fish/functions/set_theme.fish
@@ -1,4 +1,4 @@
-function set_theme --description "Set onedark theme"
+function set_theme --description "Set ansi theme"
     set -U fish_greeting
     set -U fish_key_bindings fish_hybrid_key_bindings
 
@@ -7,6 +7,32 @@ function set_theme --description "Set onedark theme"
     set -U fish_cursor_replace_one underscore
     set -U fish_cursor_visual block
 
+    set -U fish_color_autosuggestion      white
+    set -U fish_color_cancel              brblack
+    set -U fish_color_command             blue
+    set -U fish_color_comment             brblack
+    set -U fish_color_cwd                 brred
+    set -U fish_color_cwd_root            red
+    set -U fish_color_end                 white
+    set -U fish_color_error               brred
+    set -U fish_color_escape              brcyan
+    set -U fish_color_history_current     brcyan
+    set -U fish_color_host                brgreen
+    set -U fish_color_match               brcyan --underline
+    set -U fish_color_normal              white
+    set -U fish_color_operator            brmagenta
+    set -U fish_color_param               brcyan
+    set -U fish_color_quote               brgreen
+    set -U fish_color_redirection         brcyan
+    set -U fish_color_search_match        --background=blue
+    set -U fish_color_selection           --background=brblack
+    set -U fish_color_status              red
+    set -U fish_color_user                brblue
+    set -U fish_color_valid_path          brred --underline
+    set -U fish_pager_color_completion    white
+    set -U fish_pager_color_description   white
+    set -U fish_pager_color_prefix        brgreen
+    set -U fish_pager_color_progress      white
+
     set -Ux LS_COLORS (vivid generate one-dark)
-    set_onedark
 end
