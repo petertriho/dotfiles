86732fb26b87a685fecaabc8c4b2265f9949ad9e
Peter Ho <mail@petertriho.com>
2021-05-05 16:37:09 +1000

    fish: set FORGIT_LOG_GRAPH_ENABLE to true in set_theme

--- fish/.config/fish/functions/set_theme.fish
+++ fish/.config/fish/functions/set_theme.fish
@@ -36,4 +36,6 @@ function set_theme --description "Set ansi theme"
     set -U fish_pager_color_progress    white
 
     set -Ux LS_COLORS (vivid generate one-dark)
+
+    set -Ux FORGIT_LOG_GRAPH_ENABLE true
 end
