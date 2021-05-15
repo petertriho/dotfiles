227951aaff7e6ac0d1031caca1b847a27309b400
Peter Ho <mail@petertriho.com>
2021-05-15 12:52:47 +1000

    fish: change fzf colors to match tokyonight

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -54,8 +54,8 @@ if status is-interactive
       "--bind 'alt-j:preview-down,alt-k:preview-up'" \
       "--bind 'alt-a:select-all,alt-d:deselect-all'" \
       "--color=dark" \
-      "--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe" \
-      "--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
+      "--color=fg:-1,bg:-1,hl:#bb9af7,fg+:#c0caf5,bg+:#1d202f,hl+:#414868" \
+      "--color=info:#9ece6a,prompt:#7aa2f7,pointer:#f7768e,marker:#e0af68,spinner:#7dcfff,header:#7dcfff"
    set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
    set fzf_fd_opts --hidden --exclude .git
 
