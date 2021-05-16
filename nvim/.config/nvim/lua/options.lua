35180d1fdbc2e0ecb2bf33e472afb809ef8ec82c
Peter Ho <mail@petertriho.com>
2021-05-16 22:50:14 +1000

    nvim: add autocmd to highlight text

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -66,3 +66,25 @@ else
 endif
 ]]
 )
+
+local function set_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        vim.cmd('augroup ' .. group_name)
+        vim.cmd('autocmd!')
+        for _, def in pairs(definition) do
+            local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+            vim.cmd(command)
+        end
+        vim.cmd('augroup END')
+    end
+end
+
+set_augroups({
+    _general = {
+        {
+            'TextYankPost',
+            '*',
+            'lua require("vim.highlight").on_yank({ higroup = "Search", timeout = 200 })'
+        }
+    }
+})
