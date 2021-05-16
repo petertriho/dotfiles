cc118dca5bfeeb2e80696b71030bbb3aee416453
Peter Ho <mail@petertriho.com>
2021-05-16 21:49:06 +1000

    nvim: add temporary fix with scrolling error in tsx file caused by todo-comments plugin https://github.com/folke/todo-comments.nvim/issues/19

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -21,7 +21,11 @@ vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 
 -- folke/todo-comments.nvim
--- require('todo-comments').setup()
+require('todo-comments').setup {
+    highlight = {
+        comments_only = false
+    }
+}
 
 -- lukas-reineke/indent-blankline.nvim
 vim.g.indent_blankline_use_treesitter = true
