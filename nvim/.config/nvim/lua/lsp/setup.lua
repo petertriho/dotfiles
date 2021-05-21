18b3295527675224c2f561ff03bb4bc9f8fde577
Peter Tri Ho <mail@petertriho.com>
2021-05-21 13:22:42 +1000

    nvim: add lsp capabilities

--- nvim/.config/nvim/lua/lsp/setup.lua
+++ nvim/.config/nvim/lua/lsp/setup.lua
@@ -138,6 +138,9 @@ end
 local function make_base_config()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+        properties = {"documentation", "detail", "additionalTextEdits"}
+    }
     return {capabilities = capabilities, on_attach = on_attach}
 end
 
