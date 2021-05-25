cd9c22b60b61ad7eb10930f3c48c2e1f9c7f093b
Peter Tri Ho <mail@petertriho.com>
2021-05-25 10:23:52 +1000

    nvim: disable nvim-tree lsp diagnostics

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -35,7 +35,6 @@ local config = {
         vim.g.nvim_tree_follow = 1
         vim.g.nvim_tree_indent_markers = 1
         vim.g.nvim_tree_git_hl = 1
-        vim.g.nvim_tree_lsp_diagnostics = 1
     end,
     ["folke/todo-comments.nvim"] = function()
         require("todo-comments").setup()
