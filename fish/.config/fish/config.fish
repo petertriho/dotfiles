0aec6df7583177b5c17d66659ee999306c43fc6d
Peter Tri Ho <mail@petertriho.com>
2021-06-06 12:07:09 +1000

    fish: clean up config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,8 +21,8 @@ switch (uname)
         test -e /usr/local/opt/asdf/asdf.fish; and \
             source /usr/local/opt/asdf/asdf.fish
 
-        test -e {$HOME}/.iterm2_shell_integration.fish; and \
-            source {$HOME}/.iterm2_shell_integration.fish
+        test -e $HOME/.iterm2_shell_integration.fish; and \
+            source $HOME/.iterm2_shell_integration.fish
 end
 
 set -gx EDITOR nvim
@@ -30,19 +30,12 @@ set -gx PROJECT_PATHS $HOME/Documents/GitHub $HOME/Documents/Projects
 
 set -gx COMPOSE_DOCKER_CLI_BUILD 1
 
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
+#set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
+#set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
 
 if status is-interactive
     starship init fish | source
 
-    set -gx FZF_DEFAULT_COMMAND \
-        "fd" \
-        "--type file" \
-        "--color always" \
-        "--follow" \
-        "--hidden" \
-        "--exclude .git"
     set -gx FZF_DEFAULT_OPTS \
         "--ansi" \
         "--exact" \
@@ -58,7 +51,7 @@ if status is-interactive
         "--color=dark" \
         "--color=fg:-1,bg:-1,hl:#bb9af7,fg+:#c0caf5,bg+:#292e42,hl+:#414868" \
         "--color=info:#9ece6a,prompt:#7aa2f7,pointer:#f7768e,marker:#e0af68,spinner:#7dcfff,header:#7dcfff"
-    set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS
+    set -gx FORGIT_FZF_DEFAULT_OPTS $FORGIT_FZF_DEFAULT_OPTS
 
     set -gx FORGIT_GI_REPO_REMOTE https://github.com/dvcs/gitignore
     set -gx FORGIT_GI_REPO_LOCAL $HOME/.cache/forgit/gi/repos/dvcs/gitignore
@@ -72,7 +65,7 @@ if status is-interactive
     bind \e\cl __fzf_search_git_log
     bind \e\cs __fzf_search_git_status
 
-    if test "$fish_key_bindings" = fish_vi_key_bindings -o "$fish_key_bindings" = fish_hybrid_key_bindings
+    if test $fish_key_bindings = fish_vi_key_bindings -o $fish_key_bindings = fish_hybrid_key_bindings
         bind --mode insert \e\cf __fzf_search_current_dir
         bind --mode insert \e\cr __fzf_search_history
         bind --mode insert \e\cv $fzf_search_vars_cmd
