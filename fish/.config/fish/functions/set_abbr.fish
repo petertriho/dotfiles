ec625b77b8cea10c485b5bb11410923a99287397
Peter Tri Ho <mail@petertriho.com>
2021-06-16 10:59:24 +1000

    fish: add abbr for taskfile and terraform

--- fish/.config/fish/functions/set_abbr.fish
+++ fish/.config/fish/functions/set_abbr.fish
@@ -3,28 +3,30 @@ function set_abbr --description "Set abbr(eviations)"
         abbr -e $abbreviation
     end
 
-    abbr -a -U -- d   docker
-    abbr -a -U -- dc  docker compose
-    abbr -a -U -- dcl 'docker compose logs -f -t --tail=100'
-    abbr -a -U -- dcu 'docker compose up -d'
-    abbr -a -U -- g   git
-    abbr -a -U -- gb  'git branch'
-    abbr -a -U -- gc  'git commit'
-    abbr -a -U -- gco 'git checkout'
-    abbr -a -U -- gf  'git fetch'
-    abbr -a -U -- gm  'git merge'
-    abbr -a -U -- grb 'git rebase'
-    abbr -a -U -- grs 'git reset'
-    abbr -a -U -- gpl 'git pull'
-    abbr -a -U -- gps 'git push'
-    abbr -a -U -- gs  'git status'
-    abbr -a -U -- gst 'git stash'
-    abbr -a -U -- gsw 'git switch'
-    abbr -a -U -- ll  'ls -la'
-    abbr -a -U -- rmf 'rm -rf'
-    abbr -a -U -- t   tmux
-    abbr -a -U -- ta  'tmux attach-session -t'
-    abbr -a -U -- tk  'tmux kill-session -t'
-    abbr -a -U -- tl  'tmux list-sessions'
-    abbr -a -U -- tn  'tmux new-session -s'
+    abbr -a -U -- d    docker
+    abbr -a -U -- dc   'docker compose'
+    abbr -a -U -- dcl  'docker compose logs -f -t --tail=100'
+    abbr -a -U -- dcu  'docker compose up -d'
+    abbr -a -U -- g    git
+    abbr -a -U -- gb   'git branch'
+    abbr -a -U -- gc   'git commit'
+    abbr -a -U -- gco  'git checkout'
+    abbr -a -U -- gf   'git fetch'
+    abbr -a -U -- gm   'git merge'
+    abbr -a -U -- gpl  'git pull'
+    abbr -a -U -- gps  'git push'
+    abbr -a -U -- grb  'git rebase'
+    abbr -a -U -- grs  'git reset'
+    abbr -a -U -- gs   'git status'
+    abbr -a -U -- gst  'git stash'
+    abbr -a -U -- gsw  'git switch'
+    abbr -a -U -- ll   'ls -la'
+    abbr -a -U -- rmf  'rm -rf'
+    abbr -a -U -- t    tmux
+    abbr -a -U -- ta   'tmux attach-session -t'
+    abbr -a -U -- task ./Taskfile
+    abbr -a -U -- tf   terraform
+    abbr -a -U -- tk   'tmux kill-session -t'
+    abbr -a -U -- tl   'tmux list-sessions'
+    abbr -a -U -- tn   'tmux new-session -s'
 end
