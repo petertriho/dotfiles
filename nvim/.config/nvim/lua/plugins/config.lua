4763f5ad19b83f99f560bf9e8bc4f451449da912
Peter Tri Ho <mail@petertriho.com>
2021-07-19 15:43:05 +1000

    nvim: disable vimwiki table and link keymaps

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -84,6 +84,18 @@ vim.g.vimwiki_list = {
     {path = os.getenv("VIMWIKI_PATH"), syntax = "markdown", ext = ".md"}
 }
 vim.g.vimwiki_global_ext = 0
+vim.g.vimwiki_key_mappings = {
+    all_maps = 1,
+    global = 1,
+    headers = 1,
+    text_objs = 1,
+    table_format = 1,
+    table_mappings = 0,
+    lists = 1,
+    links = 0,
+    html = 1,
+    mouse = 0
+}
 
 local config = {
     ["AckslD/nvim-anywise-reg.lua"] = function()
