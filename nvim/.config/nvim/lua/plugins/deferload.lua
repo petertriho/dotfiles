e5934849c0af5be675095d549bc2dea28f0bab00
Peter Tri Ho <mail@petertriho.com>
2021-07-23 14:46:21 +1000

    nvim: rename lazy to defer

--- nvim/.config/nvim/lua/plugins/deferload.lua
+++ nvim/.config/nvim/lua/plugins/deferload.lua
@@ -1,4 +1,4 @@
-function LazyLoad()
+function DeferLoad()
     local loader = require("packer").loader
     local plugins = {
         "nvim-compe", "nvim-treesitter", "vim-matchup", "vim-unimpaired",
@@ -8,8 +8,8 @@ function LazyLoad()
     for _, plugin in pairs(plugins) do loader(plugin) end
 end
 
-vim.cmd([[autocmd User LoadLazyPlugins lua LazyLoad()]])
+vim.cmd([[autocmd User DeferLoadPlugins lua DeferLoad()]])
 
-vim.defer_fn(function() vim.cmd([[doautocmd User LoadLazyPlugins]]) end, 80)
+vim.defer_fn(function() vim.cmd([[doautocmd User DeferLoadPlugins]]) end, 80)
 
 vim.defer_fn(function() vim.cmd([[doautocmd ColorScheme]]) end, 100)
