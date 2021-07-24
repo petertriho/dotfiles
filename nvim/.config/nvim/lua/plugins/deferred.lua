4895722ef495e62ff7eb2f2e9f48028e5951e046
Peter Tri Ho <mail@petertriho.com>
2021-07-24 13:13:54 +1000

    nvim: fix deferred plugins not loading

--- nvim/.config/nvim/lua/plugins/deferred.lua
+++ nvim/.config/nvim/lua/plugins/deferred.lua
@@ -8,8 +8,8 @@ function LoadDeferredPlugins()
     for _, plugin in pairs(plugins) do loader(plugin) end
 end
 
-vim.cmd([[autocmd User LoadDeferred lua LoadDeferredPlugins()]])
+vim.cmd([[autocmd User LoadDeferredPlugins lua LoadDeferredPlugins()]])
 
-vim.defer_fn(function() vim.cmd([[doautocmd User DeferLoadPlugins]]) end, 80)
+vim.defer_fn(function() vim.cmd([[doautocmd User LoadDeferredPlugins]]) end, 80)
 
 -- vim.defer_fn(function() vim.cmd([[doautocmd ColorScheme]]) end, 100)
