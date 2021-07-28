03c4c19aa857a93081388ad4eaaa699b8153c810
Peter Tri Ho <mail@petertriho.com>
2021-07-29 09:40:00 +1000

    fish: add mk -> minikube abbr

--- fish/.config/fish/functions/set_abbr.fish
+++ fish/.config/fish/functions/set_abbr.fish
@@ -21,6 +21,7 @@ function set_abbr --description "Set abbr(eviations)"
     abbr -a -U -- gst  'git stash'
     abbr -a -U -- gsw  'git switch'
     abbr -a -U -- k    kubectl
+    abbr -a -U -- mk   minikube
     abbr -a -U -- ll   'ls -la'
     abbr -a -U -- rmf  'rm -rf'
     abbr -a -U -- t    tmux
