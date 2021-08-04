c44834b0a2e55e7d9eb3ed42d61ad058edde552e
Peter Tri Ho <mail@petertriho.com>
2021-08-04 11:35:15 +1000

    nvim: revert pyright config

--- nvim/.config/nvim/lua/lsp/config.lua
+++ nvim/.config/nvim/lua/lsp/config.lua
@@ -216,20 +216,7 @@ return {
             }
         } ]]
 	},
-	python = {
-		flags = {
-			debounce_text_changes = 150,
-		},
-		settings = {
-			python = {
-				analysis = {
-					autoSearchPaths = false,
-					useLibraryCodeForTypes = false,
-					diagnosticMode = "workspace",
-				},
-			},
-		},
-	},
+	python = {},
 	rust = {},
 	tailwindcss = {
 		filetypes = {
