227ba43a90a081614edcb50e53d6d993e6dae004
Peter Tri Ho <mail@petertriho.com>
2021-08-28 20:23:35 +1000

    nvim: disable document range formatting for tsserver

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -191,6 +191,7 @@ end
 
 local on_attach_ts = function(client, bufnr)
 	client.resolved_capabilities.document_formatting = false
+	client.resolved_capabilities.document_range_formatting = false
 
 	on_attach(client, bufnr)
 
