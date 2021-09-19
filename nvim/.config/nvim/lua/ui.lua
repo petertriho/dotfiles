430bb09c33bccc4229a607e8c348aabc1778202e
Peter Tri Ho <mail@petertriho.com>
2021-09-19 15:08:42 +1000

    nvim: fix DiagnosticSign hl

--- nvim/.config/nvim/lua/ui.lua
+++ nvim/.config/nvim/lua/ui.lua
@@ -1,12 +1,12 @@
 local signs = {
 	Error = " ",
-	Warning = " ",
+	Warn = " ",
+	Info = " ",
 	Hint = " ",
-	Information = " ",
 }
 
 for type, icon in pairs(signs) do
-	local hl = "LspDiagnosticsSign" .. type
+	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
