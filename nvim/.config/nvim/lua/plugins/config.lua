431066378cfc80ee2fd5448a6fc21bae533bd68d
Peter Tri Ho <mail@petertriho.com>
2021-09-21 14:04:59 +1000

    nvim: clean up nvim-cmp config

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -107,50 +107,49 @@ return {
 		})
 	end,
 	["hrsh7th/nvim-cmp"] = function()
-		local t = function(str)
-			return vim.api.nvim_replace_termcodes(str, true, true, true)
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 		end
 
-		--[[ local check_back_space = function()
-			local col = vim.fn.col(".") - 1
-			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
-		end ]]
+		local feedkey = function(key, mode)
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+		end
 
 		local cmp = require("cmp")
 
-		local select_prev_item = function(key)
-			return cmp.mapping(function(fallback)
-				if vim.fn.pumvisible() == 1 then
-					vim.fn.feedkeys(t("<C-p>"), "n")
-				elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-					vim.fn.feedkeys(t("<Plug>(vsnip-jump-prev)"), "")
-					--[[ elseif check_back_space() then
-					vim.fn.feedkeys(t(key), "n") ]]
-				else
-					fallback()
-				end
-			end, {
-				"i",
-				"s",
-			})
-		end
+		local select_prev_item = cmp.mapping(function(fallback)
+			if vim.fn.pumvisible() == 1 then
+				feedkey("<C-p>", "n")
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		})
 
-		local select_next_item = function(key)
-			return cmp.mapping(function(fallback)
-				if vim.fn.pumvisible() == 1 then
-					vim.fn.feedkeys(t("<C-n>"), "n")
-				elseif vim.fn["vsnip#available"](1) == 1 then
-					vim.fn.feedkeys(t("<Plug>(vsnip-expand-or-jump)"), "")
-					--[[ elseif check_back_space() then
-					vim.fn.feedkeys(t(key), "n") ]]
-				else
-					fallback()
-				end
-			end, {
-				"i",
-				"s",
-			})
-		end
+		local select_next_item = cmp.mapping(function(fallback)
+			if vim.fn.pumvisible() == 1 then
+				feedkey("<C-n>", "n")
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, {
+			"i",
+			"s",
+		})
 
 		cmp.setup({
 			formatting = {
@@ -179,10 +178,10 @@ return {
 					behavior = cmp.ConfirmBehavior.Replace,
 					select = true,
 				}), ]]
-				["<C-k>"] = select_prev_item("<C-k>"),
-				["<C-j>"] = select_next_item("<C-j>"),
-				["<S-Tab>"] = select_prev_item("<S-Tab>"),
-				["<Tab>"] = select_next_item("<Tab>"),
+				["<C-k>"] = select_prev_item,
+				["<C-j>"] = select_next_item,
+				["<S-Tab>"] = select_prev_item,
+				["<Tab>"] = select_next_item,
 			},
 			snippet = {
 				expand = function(args)
@@ -549,7 +548,7 @@ return {
 			map_cr = true,
 			map_complete = false,
 			auto_select = false,
-            insert = false
+			insert = false,
 		})
 	end,
 	["windwp/nvim-ts-autotag"] = function()
