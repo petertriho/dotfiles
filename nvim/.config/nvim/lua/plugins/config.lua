7d03e643475ee363aebd0fe094990c3d8a92fbcd
Peter Tri Ho <mail@petertriho.com>
2021-09-23 09:33:13 +1000

    nvim: fix tabout not working

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -106,13 +106,13 @@ return {
 		})
 	end,
 	["hrsh7th/nvim-cmp"] = function()
-		local has_words_before = function()
+		--[[ local has_words_before = function()
 			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 				return false
 			end
 			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
+		end ]]
 
 		local feedkey = function(key, mode)
 			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
@@ -125,8 +125,8 @@ return {
 				feedkey("<C-p>", "n")
 			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
 				feedkey("<Plug>(vsnip-jump-prev)", "")
-			elseif has_words_before() then
-				cmp.complete()
+				--[[ elseif has_words_before() then
+				cmp.complete() ]]
 			else
 				fallback()
 			end
@@ -140,8 +140,8 @@ return {
 				feedkey("<C-n>", "n")
 			elseif vim.fn["vsnip#available"](1) == 1 then
 				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
+				--[[ elseif has_words_before() then
+				cmp.complete() ]]
 			else
 				fallback()
 			end
