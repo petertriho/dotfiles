925acf239186342bb552fbd04f1099c7c8c54299
Peter Tri Ho <mail@petertriho.com>
2021-09-30 09:01:41 +1000

    wezterm: fix separate configs for different platforms

--- ignore/wezterm/wezterm.lua
+++ ignore/wezterm/wezterm.lua
@@ -1,10 +1,9 @@
 local wezterm = require("wezterm")
 
-return {
-	default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "\\\\wsl$\\Ubuntu\\home\\peter" },
-	font = wezterm.font("JetBrainsMono NF"),
+local config = {
 	font_size = 13,
 	color_scheme = "tokyonight",
+	bold_brightens_ansi_colors = true,
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	window_close_confirmation = "NeverPrompt",
 	hide_tab_bar_if_only_one_tab = true,
@@ -33,10 +32,19 @@ return {
 		{ mods = "LEADER", key = "x", action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
 		{ mods = "LEADER", key = "z", action = "TogglePaneZoomState" },
 		{ mods = "LEADER|CTRL", key = "s", action = wezterm.action({ SendString = "\x13" }) },
+		{ mods = "LEADER|SHIFT", key = "&", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
 		{ mods = "LEADER|SHIFT", key = "H", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
 		{ mods = "LEADER|SHIFT", key = "J", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
 		{ mods = "LEADER|SHIFT", key = "K", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
 		{ mods = "LEADER|SHIFT", key = "L", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-		{ mods = "LEADER|SHIFT", key = "x", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
 	},
 }
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "\\\\wsl$\\Ubuntu\\home\\peter" }
+	config.font = wezterm.font("JetBrainsMono NF")
+else
+	config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+end
+
+return config
