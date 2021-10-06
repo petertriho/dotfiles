859784e21625cd0b307f999ff10d1f3dbf829d2a
Peter Tri Ho <mail@petertriho.com>
2021-10-07 09:41:57 +1100

    fish: add `$HOME/.local/share/man` to `MANPATH`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,6 +30,8 @@ set -gx PROJECT_PATHS $HOME/Documents/GitHub $HOME/Documents/Projects
 
 set -gx COMPOSE_DOCKER_CLI_BUILD 1
 
+set -gx MANPATH "$HOME/.local/share/man:$MANPATH"
+
 if status is-interactive
     if command -v starship &>/dev/null
         starship init fish | source
