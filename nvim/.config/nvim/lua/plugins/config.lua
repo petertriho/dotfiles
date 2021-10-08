d3a73e26795fe153e4ed6fb14e572c9f22a68eef
Peter Tri Ho <mail@petertriho.com>
2021-10-08 12:03:23 +1100

    nvim: prefer single line comments

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -41,9 +41,14 @@ return {
 		vim.g.cursorhold_updatetime = 100
 	end,
 	["b3nj5m1n/kommentary"] = function()
+		require("kommentary.config").configure_language("default", {
+			prefer_single_line_comments = true,
+		})
+
 		require("kommentary.config").configure_language("typescriptreact", {
 			single_line_comment_string = "auto",
 			multi_line_comment_strings = "auto",
+			prefer_single_line_comments = true,
 			hook_function = function()
 				require("ts_context_commentstring.internal").update_commentstring()
 			end,
