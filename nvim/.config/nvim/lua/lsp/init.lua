23850551665180412d2480dd32eaad51bf956218
Peter Tri Ho <mail@petertriho.com>
2021-10-09 14:35:41 +1100

    nvim: disable nvim-lsputils code_action_handler

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -149,7 +149,7 @@ local function setup()
 		vim.lsp.handlers.signature_help,
 		{ border = "single" }
 	)
-	vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
+	-- vim.lsp.handlers["textDocument/codeAction"] = require("lsputil.codeAction").code_action_handler
 	vim.lsp.handlers["textDocument/references"] = require("lsputil.locations").references_handler
 	vim.lsp.handlers["textDocument/definition"] = require("lsputil.locations").definition_handler
 	vim.lsp.handlers["textDocument/declaration"] = require("lsputil.locations").declaration_handler
