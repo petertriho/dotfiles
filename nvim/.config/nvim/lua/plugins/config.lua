581af853d33865df27f83d928ecbcdeb0a7c4419
Peter Tri Ho <mail@petertriho.com>
2021-10-17 00:42:10 +1100

    feat(nvim): add arrow function to autopairs

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -809,7 +809,17 @@ return {
     end,
     ["windwp/nvim-autopairs"] = function()
         require("nvim-treesitter.configs").setup({ autopairs = { enable = true } })
-        require("nvim-autopairs").setup({ check_ts = true })
+
+        local autopairs = require("nvim-autopairs")
+        autopairs.setup()
+
+        local Rule = require("nvim-autopairs.rule")
+        autopairs.add_rules({
+            Rule("%(.*%)%s*%=$", "> {}", { "typescript", "typescriptreact", "javascript", "javascriptreact" })
+                :use_regex(true)
+                :set_end_pair_length(1),
+        })
+
         require("nvim-autopairs.completion.cmp").setup({
             map_cr = true,
             map_complete = false,
