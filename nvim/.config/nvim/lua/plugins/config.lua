8189c8ff9c6bdaf0d89c7ac51e47fc8bfd3ad8d2
Peter Tri Ho <mail@petertriho.com>
2021-10-16 14:27:48 +1100

    fix(nvim): remove broken Gpush/Gfetch commands

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -18,12 +18,6 @@ function! ToggleNeogitStatus() abort
 endfunction
 
 command! -bang -nargs=* -complete=file Make AsyncRun -program=make @ <args>
-
-command! -bang -bar -nargs=* Gpush execute 'AsyncRun<bang> -cwd=' .
-          \ fnameescape(FugitiveGitDir()) 'git push' <q-args>
-
-command! -bang -bar -nargs=* Gfetch execute 'AsyncRun<bang> -cwd=' .
-          \ fnameescape(FugitiveGitDir()) 'git fetch' <q-args>
 ]])
 
 return {
