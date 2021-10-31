c30dd22d54873013c67b622922093d92e586a8ea
Peter Tri Ho <mail@petertriho.com>
2021-11-01 00:11:45 +1100

    style(wezterm): format config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,94 +1,94 @@
 local wezterm = require("wezterm")
 
 local config = {
-	color_scheme = "tokyonight",
-	bold_brightens_ansi_colors = true,
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-	window_close_confirmation = "NeverPrompt",
-	hide_tab_bar_if_only_one_tab = true,
-	leader = { key = "b", mods = "CTRL" },
-	keys = {
-		{
-			mods = "LEADER",
-			key = "\\",
-			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-		},
-		{ mods = "LEADER", key = "-", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-		{ mods = "LEADER", key = "[", action = "ActivateCopyMode" },
-		{ mods = "LEADER", key = "1", action = wezterm.action({ ActivateTab = 0 }) },
-		{ mods = "LEADER", key = "2", action = wezterm.action({ ActivateTab = 1 }) },
-		{ mods = "LEADER", key = "3", action = wezterm.action({ ActivateTab = 2 }) },
-		{ mods = "LEADER", key = "4", action = wezterm.action({ ActivateTab = 3 }) },
-		{ mods = "LEADER", key = "5", action = wezterm.action({ ActivateTab = 4 }) },
-		{ mods = "LEADER", key = "6", action = wezterm.action({ ActivateTab = 5 }) },
-		{ mods = "LEADER", key = "7", action = wezterm.action({ ActivateTab = 6 }) },
-		{ mods = "LEADER", key = "8", action = wezterm.action({ ActivateTab = 7 }) },
-		{ mods = "LEADER", key = "9", action = wezterm.action({ ActivateTab = 8 }) },
-		{ mods = "LEADER", key = "Tab", action = "QuickSelect" },
-		{ mods = "LEADER", key = "a", action = "ShowTabNavigator" },
-		{ mods = "LEADER", key = "c", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-		{ mods = "LEADER", key = "h", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-		{ mods = "LEADER", key = "j", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-		{ mods = "LEADER", key = "k", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-		{ mods = "LEADER", key = "l", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-		{ mods = "LEADER", key = "n", action = wezterm.action({ ActivateTabRelative = 1 }) },
-		{ mods = "LEADER", key = "p", action = wezterm.action({ ActivateTabRelative = -1 }) },
-		{ mods = "LEADER", key = "s", action = "ActivateLastTab" },
-		{ mods = "LEADER", key = "x", action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
-		{ mods = "LEADER", key = "z", action = "TogglePaneZoomState" },
-		{ mods = "LEADER|CTRL", key = "s", action = wezterm.action({ SendString = "\x13" }) },
-		{ mods = "LEADER|SHIFT", key = "&", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
-		{ mods = "LEADER|SHIFT", key = "H", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-		{ mods = "LEADER|SHIFT", key = "J", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-		{ mods = "LEADER|SHIFT", key = "K", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-		{ mods = "LEADER|SHIFT", key = "L", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-	},
-	colors = {
-		tab_bar = {
-			background = "#0E0E14",
-			active_tab = {
-				bg_color = "#1A1B26",
-				fg_color = "#C0CAF5",
-			},
-			inactive_tab = {
-				bg_color = "#13141C",
-				fg_color = "#565F89",
-			},
-			inactive_tab_hover = {
-				bg_color = "#1A1B26",
-				fg_color = "#C0CAF5",
-			},
-			new_tab = {
-				bg_color = "#13141C",
-				fg_color = "#565F89",
-			},
-			new_tab_hover = {
-				bg_color = "#1A1B26",
-				fg_color = "#C0CAF5",
-			},
-		},
-	},
+    color_scheme = "tokyonight",
+    bold_brightens_ansi_colors = true,
+    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+    window_close_confirmation = "NeverPrompt",
+    hide_tab_bar_if_only_one_tab = true,
+    leader = { key = "b", mods = "CTRL" },
+    keys = {
+        {
+            mods = "LEADER",
+            key = "\\",
+            action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+        },
+        { mods = "LEADER", key = "-", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+        { mods = "LEADER", key = "[", action = "ActivateCopyMode" },
+        { mods = "LEADER", key = "1", action = wezterm.action({ ActivateTab = 0 }) },
+        { mods = "LEADER", key = "2", action = wezterm.action({ ActivateTab = 1 }) },
+        { mods = "LEADER", key = "3", action = wezterm.action({ ActivateTab = 2 }) },
+        { mods = "LEADER", key = "4", action = wezterm.action({ ActivateTab = 3 }) },
+        { mods = "LEADER", key = "5", action = wezterm.action({ ActivateTab = 4 }) },
+        { mods = "LEADER", key = "6", action = wezterm.action({ ActivateTab = 5 }) },
+        { mods = "LEADER", key = "7", action = wezterm.action({ ActivateTab = 6 }) },
+        { mods = "LEADER", key = "8", action = wezterm.action({ ActivateTab = 7 }) },
+        { mods = "LEADER", key = "9", action = wezterm.action({ ActivateTab = 8 }) },
+        { mods = "LEADER", key = "Tab", action = "QuickSelect" },
+        { mods = "LEADER", key = "a", action = "ShowTabNavigator" },
+        { mods = "LEADER", key = "c", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+        { mods = "LEADER", key = "h", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+        { mods = "LEADER", key = "j", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+        { mods = "LEADER", key = "k", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+        { mods = "LEADER", key = "l", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+        { mods = "LEADER", key = "n", action = wezterm.action({ ActivateTabRelative = 1 }) },
+        { mods = "LEADER", key = "p", action = wezterm.action({ ActivateTabRelative = -1 }) },
+        { mods = "LEADER", key = "s", action = "ActivateLastTab" },
+        { mods = "LEADER", key = "x", action = wezterm.action({ CloseCurrentPane = { confirm = false } }) },
+        { mods = "LEADER", key = "z", action = "TogglePaneZoomState" },
+        { mods = "LEADER|CTRL", key = "s", action = wezterm.action({ SendString = "\x13" }) },
+        { mods = "LEADER|SHIFT", key = "&", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
+        { mods = "LEADER|SHIFT", key = "H", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+        { mods = "LEADER|SHIFT", key = "J", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+        { mods = "LEADER|SHIFT", key = "K", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+        { mods = "LEADER|SHIFT", key = "L", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+    },
+    colors = {
+        tab_bar = {
+            background = "#0E0E14",
+            active_tab = {
+                bg_color = "#1A1B26",
+                fg_color = "#C0CAF5",
+            },
+            inactive_tab = {
+                bg_color = "#13141C",
+                fg_color = "#565F89",
+            },
+            inactive_tab_hover = {
+                bg_color = "#1A1B26",
+                fg_color = "#C0CAF5",
+            },
+            new_tab = {
+                bg_color = "#13141C",
+                fg_color = "#565F89",
+            },
+            new_tab_hover = {
+                bg_color = "#1A1B26",
+                fg_color = "#C0CAF5",
+            },
+        },
+    },
 }
 
 wezterm.on("format-tab-title", function(tab)
-	if tab.is_active then
-		return {
-			{ Text = "▎" .. tab.active_pane.title },
-		}
-	end
+    if tab.is_active then
+        return {
+            { Text = "▎" .. tab.active_pane.title },
+        }
+    end
 
-	return {
-		{ Text = " " .. tab.active_pane.title },
-	}
+    return {
+        { Text = " " .. tab.active_pane.title },
+    }
 end)
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "\\\\wsl$\\Ubuntu\\home\\peter" }
-	config.font = wezterm.font("JetBrainsMono NF")
-	config.font_size = 13
+    config.default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "\\\\wsl$\\Ubuntu\\home\\peter" }
+    config.font = wezterm.font("JetBrainsMono NF")
+    config.font_size = 13
 else
-	config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-	config.font_size = 16
+    config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+    config.font_size = 16
 end
 
 return config
