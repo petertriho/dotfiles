a8c9b1dbfd47d45adcfb0f2063172a83370b8a54
Peter Tri Ho <mail@petertriho.com>
2021-11-25 15:56:22 +1100

    Revert "refactor(nvim): rename scrollbar marks to signs"
    This reverts commit cbb5197ad3273bf2dbafda41bda39d7a68872b25.

--- nvim/.config/nvim/lua/scrollbar.lua
+++ nvim/.config/nvim/lua/scrollbar.lua
@@ -7,24 +7,17 @@ local NAME_SUFFIX = "Handle"
 
 local NAMESPACE = vim.api.nvim_create_namespace(NAME_PREFIX)
 
-local SIGNS = { "-", "=" }
+local MARKS = { "-", "=" }
 
-local SIGN_TYPES = {
-    ERROR = "Error",
-    WARN = "Warn",
-    INFO = "Info",
-    HINT = "Hint",
+local MARK_TYPE_PRIORITY = {
+    Error = 1,
+    Warn = 2,
+    Info = 3,
+    Hint = 4,
 }
 
-local SIGN_TYPE_PRIORITY = {
-    [SIGN_TYPES.ERROR] = 1,
-    [SIGN_TYPES.WARN] = 2,
-    [SIGN_TYPES.INFO] = 3,
-    [SIGN_TYPES.HINT] = 4,
-}
-
-local get_highlight_name = function(sign_type, handle)
-    return string.format("%s%s%s", NAME_PREFIX, sign_type, handle and NAME_SUFFIX or "")
+local get_highlight_name = function(mark_type, handle)
+    return string.format("%s%s%s", NAME_PREFIX, mark_type, handle and NAME_SUFFIX or "")
 end
 
 M.render = function()
@@ -48,38 +41,38 @@ M.render = function()
     local relative_first_line = math.floor(first_visible_line * ratio) - math.floor(1 * ratio)
     local relative_last_line = math.floor(last_visible_line * ratio)
 
-    local scrollbar_signs = vim.b.scrollbar_signs or {}
+    local scrollbar_marks = vim.b.scrollbar_marks or {}
 
-    local handle_signs = {}
-    local other_signs = {}
+    local handle_marks = {}
+    local other_marks = {}
 
-    for _, namespace_signs in pairs(scrollbar_signs) do
-        table.sort(namespace_signs, function(a, b)
+    for _, namespace_marks in pairs(scrollbar_marks) do
+        table.sort(namespace_marks, function(a, b)
             if a.line == b.line then
-                return SIGN_TYPE_PRIORITY[a.type] < SIGN_TYPE_PRIORITY[b.type]
+                return MARK_TYPE_PRIORITY[a.type] < MARK_TYPE_PRIORITY[b.type]
             end
             return a.line < b.line
         end)
 
-        for _, sign in pairs(namespace_signs) do
-            local relative_sign_line = math.floor(sign.line * ratio)
+        for _, mark in pairs(namespace_marks) do
+            local relative_mark_line = math.floor(mark.line * ratio)
 
-            if sign.line <= total_lines then
+            if mark.line <= total_lines then
                 if
-                    handle_signs[#handle_signs]
-                    and math.floor(handle_signs[#handle_signs].line * ratio) == relative_sign_line
+                    handle_marks[#handle_marks]
+                    and math.floor(handle_marks[#handle_marks].line * ratio) == relative_mark_line
                 then
-                    handle_signs[#handle_signs].text = SIGNS[2]
+                    handle_marks[#handle_marks].text = MARKS[2]
                 elseif
-                    other_signs[#other_signs]
-                    and math.floor(other_signs[#other_signs].line * ratio) == relative_sign_line
+                    other_marks[#other_marks]
+                    and math.floor(other_marks[#other_marks].line * ratio) == relative_mark_line
                 then
-                    other_signs[#other_signs].text = SIGNS[2]
+                    other_marks[#other_marks].text = MARKS[2]
                 else
-                    if relative_sign_line >= relative_first_line and relative_sign_line <= relative_last_line then
-                        table.insert(handle_signs, sign)
+                    if relative_mark_line >= relative_first_line and relative_mark_line <= relative_last_line then
+                        table.insert(handle_marks, mark)
                     else
-                        table.insert(other_signs, sign)
+                        table.insert(other_marks, mark)
                     end
                 end
             end
@@ -91,20 +84,20 @@ M.render = function()
             virt_text_pos = "right_align",
         }
 
-        local handle_sign = nil
+        local handle_mark = nil
 
-        for index, sign in ipairs(handle_signs) do
-            local relative_sign_line = math.floor(sign.line * ratio)
-            if relative_sign_line >= i - 1 and relative_sign_line <= i then
-                handle_sign = sign
-                table.remove(handle_signs, index)
+        for index, mark in ipairs(handle_marks) do
+            local relative_mark_line = math.floor(mark.line * ratio)
+            if relative_mark_line >= i - 1 and relative_mark_line <= i then
+                handle_mark = mark
+                table.remove(handle_marks, index)
                 break
             end
         end
 
-        if handle_sign then
+        if handle_mark then
             handle_opts.virt_text = {
-                { handle_sign.text, get_highlight_name(handle_sign.type, true) },
+                { handle_mark.text, get_highlight_name(handle_mark.type, true) },
             }
         else
             handle_opts.virt_text = {
@@ -112,19 +105,19 @@ M.render = function()
             }
         end
 
-        vim.api.nvim_buf_set_extsign(0, NAMESPACE, first_visible_line - 1 + i, -1, handle_opts)
+        vim.api.nvim_buf_set_extmark(0, NAMESPACE, first_visible_line - 1 + i, -1, handle_opts)
     end
 
-    for _, sign in pairs(other_signs) do
-        if sign ~= nil then
+    for _, mark in pairs(other_marks) do
+        if mark ~= nil then
             local handle_opts = {
                 virt_text_pos = "right_align",
-                virt_text = { { sign.text, get_highlight_name(sign.type, false) } },
+                virt_text = { { mark.text, get_highlight_name(mark.type, false) } },
             }
-            vim.api.nvim_buf_set_extsign(
+            vim.api.nvim_buf_set_extmark(
                 0,
                 NAMESPACE,
-                first_visible_line - 1 + math.floor(tonumber(sign.line) * ratio),
+                first_visible_line - 1 + math.floor(tonumber(mark.line) * ratio),
                 -1,
                 handle_opts
             )
@@ -132,27 +125,27 @@ M.render = function()
     end
 end
 
-local diagnostics_sign_properties = {
-    [vim.diagnostic.severity.ERROR] = { text = SIGNS[1], type = SIGN_TYPES.ERROR },
-    [vim.diagnostic.severity.WARN] = { text = SIGNS[1], type = SIGN_TYPES.WARN },
-    [vim.diagnostic.severity.INFO] = { text = SIGNS[1], type = SIGN_TYPES.INFO },
-    [vim.diagnostic.severity.HINT] = { text = SIGNS[1], type = SIGN_TYPES.HINT },
+local diagnostics_mark_properties = {
+    [vim.diagnostic.severity.ERROR] = { text = MARKS[1], type = "Error" },
+    [vim.diagnostic.severity.WARN] = { text = MARKS[1], type = "Warn" },
+    [vim.diagnostic.severity.INFO] = { text = MARKS[1], type = "Info" },
+    [vim.diagnostic.severity.HINT] = { text = MARKS[2], type = "Hint" },
 }
 
 M.diagnostics_handler = function(err, result, ctx, config)
     local bufnr = vim.uri_to_bufnr(result.uri)
 
-    local diagnostics_scrollbar_signs = {}
+    local diagnostics_scrollbar_marks = {}
 
     for _, diagnostic in pairs(result.diagnostics) do
-        table.insert(diagnostics_scrollbar_signs, {
+        table.insert(diagnostics_scrollbar_marks, {
             line = diagnostic.range.start.line,
-            text = diagnostics_sign_properties[diagnostic.severity].text,
-            type = diagnostics_sign_properties[diagnostic.severity].type,
+            text = diagnostics_mark_properties[diagnostic.severity].text,
+            type = diagnostics_mark_properties[diagnostic.severity].type,
         })
     end
 
-    vim.api.nvim_buf_set_var(bufnr, "scrollbar_signs", { diagnostics = diagnostics_scrollbar_signs })
+    vim.api.nvim_buf_set_var(bufnr, "scrollbar_marks", { diagnostics = diagnostics_scrollbar_marks })
 
     if vim.uri_from_bufnr(0) == result.uri then
         M.render()
@@ -162,14 +155,14 @@ end
 M.setup = function()
     local highlights = {
         [NAME_PREFIX .. NAME_SUFFIX] = { "NONE", colors.bg_highlight },
-        [NAME_PREFIX .. SIGN_TYPES.ERROR .. NAME_SUFFIX] = { colors.error, colors.bg_highlight },
-        [NAME_PREFIX .. SIGN_TYPES.WARN .. NAME_SUFFIX] = { colors.warning, colors.bg_highlight },
-        [NAME_PREFIX .. SIGN_TYPES.INFO .. NAME_SUFFIX] = { colors.info, colors.bg_highlight },
-        [NAME_PREFIX .. SIGN_TYPES.HINT .. NAME_SUFFIX] = { colors.hint, colors.bg_highlight },
-        [NAME_PREFIX .. SIGN_TYPES.ERROR] = { colors.error, "NONE" },
-        [NAME_PREFIX .. SIGN_TYPES.WARN] = { colors.warning, "NONE" },
-        [NAME_PREFIX .. SIGN_TYPES.INFO] = { colors.info, "NONE" },
-        [NAME_PREFIX .. SIGN_TYPES.HINT] = { colors.hint, "NONE" },
+        [NAME_PREFIX .. "Error" .. NAME_SUFFIX] = { colors.error, colors.bg_highlight },
+        [NAME_PREFIX .. "Warn" .. NAME_SUFFIX] = { colors.warning, colors.bg_highlight },
+        [NAME_PREFIX .. "Info" .. NAME_SUFFIX] = { colors.info, colors.bg_highlight },
+        [NAME_PREFIX .. "Hint" .. NAME_SUFFIX] = { colors.hint, colors.bg_highlight },
+        [NAME_PREFIX .. "Error"] = { colors.error, "NONE" },
+        [NAME_PREFIX .. "Warn"] = { colors.warning, "NONE" },
+        [NAME_PREFIX .. "Info"] = { colors.info, "NONE" },
+        [NAME_PREFIX .. "Hint"] = { colors.hint, "NONE" },
     }
 
     for name, highlight in pairs(highlights) do
