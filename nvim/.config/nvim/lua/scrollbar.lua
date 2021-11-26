26493e4058627149ab34d7e586af06049e12ad9b
Peter Tri Ho <mail@petertriho.com>
2021-11-26 14:00:21 +1100

    feat(nvim): add excluded_filetypes to scrollbar

--- nvim/.config/nvim/lua/scrollbar.lua
+++ nvim/.config/nvim/lua/scrollbar.lua
@@ -16,6 +16,21 @@ local MARK_TYPE_PRIORITY = {
     Hint = 4,
 }
 
+local EXCLUDED_FILETYPES = {
+    "",
+    "prompt",
+    "dbui",
+    "NvimTree",
+    "lspinfo",
+    "Mundo",
+    "MundoDiff",
+    "packer",
+    "fugitive",
+    "fugitiveblame",
+    "NeogitStatus",
+    "DiffViewFiles",
+}
+
 local get_highlight_name = function(mark_type, handle)
     return string.format("%s%s%s", NAME_PREFIX, mark_type, handle and NAME_SUFFIX or "")
 end
@@ -23,103 +38,105 @@ end
 M.render = function()
     vim.api.nvim_buf_clear_namespace(0, NAMESPACE, 0, -1)
 
-    local total_lines = vim.api.nvim_buf_line_count(0)
-    local visible_lines = vim.api.nvim_win_get_height(0)
-    local first_visible_line = vim.fn.line("w0")
-    local last_visible_line = vim.fn.line("w$")
+    if not vim.tbl_contains(EXCLUDED_FILETYPES, vim.bo.filetype) then
+        local total_lines = vim.api.nvim_buf_line_count(0)
+        local visible_lines = vim.api.nvim_win_get_height(0)
+        local first_visible_line = vim.fn.line("w0")
+        local last_visible_line = vim.fn.line("w$")
 
-    if visible_lines >= total_lines then
-        visible_lines = total_lines
-    end
+        if visible_lines >= total_lines then
+            visible_lines = total_lines
+        end
 
-    local ratio = visible_lines / total_lines
+        local ratio = visible_lines / total_lines
 
-    local relative_first_line = math.floor(first_visible_line * ratio) - math.floor(1 * ratio)
-    local relative_last_line = math.floor(last_visible_line * ratio)
+        local relative_first_line = math.floor(first_visible_line * ratio) - math.floor(1 * ratio)
+        local relative_last_line = math.floor(last_visible_line * ratio)
 
-    local scrollbar_marks = vim.b.scrollbar_marks or {}
+        local scrollbar_marks = vim.b.scrollbar_marks or {}
 
-    local handle_marks = {}
-    local other_marks = {}
+        local handle_marks = {}
+        local other_marks = {}
 
-    for _, namespace_marks in pairs(scrollbar_marks) do
-        table.sort(namespace_marks, function(a, b)
-            if a.line == b.line then
-                return MARK_TYPE_PRIORITY[a.type] < MARK_TYPE_PRIORITY[b.type]
-            end
-            return a.line < b.line
-        end)
+        for _, namespace_marks in pairs(scrollbar_marks) do
+            table.sort(namespace_marks, function(a, b)
+                if a.line == b.line then
+                    return MARK_TYPE_PRIORITY[a.type] < MARK_TYPE_PRIORITY[b.type]
+                end
+                return a.line < b.line
+            end)
 
-        for _, mark in pairs(namespace_marks) do
-            local relative_mark_line = math.floor(mark.line * ratio)
+            for _, mark in pairs(namespace_marks) do
+                local relative_mark_line = math.floor(mark.line * ratio)
 
-            if mark.line <= total_lines then
-                if
-                    handle_marks[#handle_marks]
-                    and math.floor(handle_marks[#handle_marks].line * ratio) == relative_mark_line
-                then
-                    handle_marks[#handle_marks].text = MARKS[2]
-                elseif
-                    other_marks[#other_marks]
-                    and math.floor(other_marks[#other_marks].line * ratio) == relative_mark_line
-                then
-                    other_marks[#other_marks].text = MARKS[2]
-                else
-                    if relative_mark_line >= relative_first_line and relative_mark_line <= relative_last_line then
-                        table.insert(handle_marks, mark)
+                if mark.line <= total_lines then
+                    if
+                        handle_marks[#handle_marks]
+                        and math.floor(handle_marks[#handle_marks].line * ratio) == relative_mark_line
+                    then
+                        handle_marks[#handle_marks].text = MARKS[2]
+                    elseif
+                        other_marks[#other_marks]
+                        and math.floor(other_marks[#other_marks].line * ratio) == relative_mark_line
+                    then
+                        other_marks[#other_marks].text = MARKS[2]
                     else
-                        table.insert(other_marks, mark)
+                        if relative_mark_line >= relative_first_line and relative_mark_line <= relative_last_line then
+                            table.insert(handle_marks, mark)
+                        else
+                            table.insert(other_marks, mark)
+                        end
                     end
                 end
             end
         end
-    end
 
-    local scroll_offset = visible_lines - (last_visible_line - first_visible_line)
+        local scroll_offset = visible_lines - (last_visible_line - first_visible_line)
 
-    for i = relative_first_line, relative_last_line, 1 do
-        local mark_line = first_visible_line + i - scroll_offset
-
-        if mark_line >= 0 then
-            local handle_opts = {
-                virt_text_pos = "right_align",
-            }
-
-            local handle_mark = nil
-
-            for index, mark in ipairs(handle_marks) do
-                local relative_mark_line = math.floor(mark.line * ratio)
-                if relative_mark_line >= i - 1 and relative_mark_line <= i then
-                    handle_mark = mark
-                    table.remove(handle_marks, index)
-                    break
-                end
-            end
-
-            if handle_mark then
-                handle_opts.virt_text = {
-                    { handle_mark.text, get_highlight_name(handle_mark.type, true) },
-                }
-            else
-                handle_opts.virt_text = {
-                    { " ", get_highlight_name("", true) },
-                }
-            end
-
-            vim.api.nvim_buf_set_extmark(0, NAMESPACE, mark_line, -1, handle_opts)
-        end
-    end
-
-    for _, mark in pairs(other_marks) do
-        if mark ~= nil then
-            local mark_line = first_visible_line + math.floor(tonumber(mark.line) * ratio) - scroll_offset
+        for i = relative_first_line, relative_last_line, 1 do
+            local mark_line = first_visible_line + i - scroll_offset
 
             if mark_line >= 0 then
-                local mark_opts = {
+                local handle_opts = {
                     virt_text_pos = "right_align",
-                    virt_text = { { mark.text, get_highlight_name(mark.type, false) } },
                 }
-                vim.api.nvim_buf_set_extmark(0, NAMESPACE, mark_line, -1, mark_opts)
+
+                local handle_mark = nil
+
+                for index, mark in ipairs(handle_marks) do
+                    local relative_mark_line = math.floor(mark.line * ratio)
+                    if relative_mark_line >= i - 1 and relative_mark_line <= i then
+                        handle_mark = mark
+                        table.remove(handle_marks, index)
+                        break
+                    end
+                end
+
+                if handle_mark then
+                    handle_opts.virt_text = {
+                        { handle_mark.text, get_highlight_name(handle_mark.type, true) },
+                    }
+                else
+                    handle_opts.virt_text = {
+                        { " ", get_highlight_name("", true) },
+                    }
+                end
+
+                vim.api.nvim_buf_set_extmark(0, NAMESPACE, mark_line, -1, handle_opts)
+            end
+        end
+
+        for _, mark in pairs(other_marks) do
+            if mark ~= nil then
+                local mark_line = first_visible_line + math.floor(tonumber(mark.line) * ratio) - scroll_offset
+
+                if mark_line >= 0 then
+                    local mark_opts = {
+                        virt_text_pos = "right_align",
+                        virt_text = { { mark.text, get_highlight_name(mark.type, false) } },
+                    }
+                    vim.api.nvim_buf_set_extmark(0, NAMESPACE, mark_line, -1, mark_opts)
+                end
             end
         end
     end
@@ -134,21 +151,24 @@ local diagnostics_mark_properties = {
 
 M.diagnostics_handler = function(_, result, _, _)
     local bufnr = vim.uri_to_bufnr(result.uri)
+    local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
 
-    local diagnostics_scrollbar_marks = {}
+    if not vim.tbl_contains(EXCLUDED_FILETYPES, filetype) then
+        local diagnostics_scrollbar_marks = {}
 
-    for _, diagnostic in pairs(result.diagnostics) do
-        table.insert(diagnostics_scrollbar_marks, {
-            line = diagnostic.range.start.line,
-            text = diagnostics_mark_properties[diagnostic.severity].text,
-            type = diagnostics_mark_properties[diagnostic.severity].type,
-        })
-    end
+        for _, diagnostic in pairs(result.diagnostics) do
+            table.insert(diagnostics_scrollbar_marks, {
+                line = diagnostic.range.start.line,
+                text = diagnostics_mark_properties[diagnostic.severity].text,
+                type = diagnostics_mark_properties[diagnostic.severity].type,
+            })
+        end
 
-    vim.api.nvim_buf_set_var(bufnr, "scrollbar_marks", { diagnostics = diagnostics_scrollbar_marks })
+        vim.api.nvim_buf_set_var(bufnr, "scrollbar_marks", { diagnostics = diagnostics_scrollbar_marks })
 
-    if vim.uri_from_bufnr(0) == result.uri then
-        M.render()
+        if vim.uri_from_bufnr(0) == result.uri then
+            M.render()
+        end
     end
 end
 
