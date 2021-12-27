73167602cc8511ddb1992bfcf49d01f7d0fcd6c4
Peter Tri Ho <mail@petertriho.com>
2021-12-28 01:20:54 +1100

    feat(nvim): add keymaps for word range substitution

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -215,14 +215,17 @@ return {
         set_keymap("x", "<Leader>s", "<CMD>lua require('substitute').visual()<CR>", { noremap = true })
 
         set_keymap("n", "\\s", "<CMD>lua require('substitute.range').operator()<CR>", { noremap = true })
-        set_keymap("x", "\\s", "<CMD>lua require('substitute.range').visual()<CR>", { noremap = true })
+        set_keymap("x", "\\s", "<CMD>lua require('substitute.range').visual()<CR>", {})
+        set_keymap("n", "\\ss", "<CMD>lua require('substitute.range').word()<CR>", {})
+
         set_keymap(
             "n",
             "\\S",
             "<CMD>lua require('substitute.range').operator({ prefix = 'S' })<CR>",
             { noremap = true }
         )
-        set_keymap("x", "\\S", "<CMD>lua require('substitute.range').visual({ prefix = 'S' })<CR>", { noremap = true })
+        set_keymap("x", "\\S", "<CMD>lua require('substitute.range').visual({ prefix = 'S' })<CR>", {})
+        set_keymap("n", "\\SS", "<CMD>lua require('substitute.range').word({ prefix = 'S' })<CR>", {})
 
         set_keymap("x", "p", "<CMD>lua require('substitute').visual()<CR>", {})
         set_keymap("x", "P", "<CMD>lua require('substitute').visual()<CR>", {})
