b71e6fa31973331689c8eb0668d168f5ea8b76d8
Peter Tri Ho <mail@petertriho.com>
2021-12-28 12:23:02 +1100

    feat(nvim): disable lightspeed exit after idle for unlabeled jumps

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -109,6 +109,7 @@ return {
     end,
     ["ggandor/lightspeed.nvim"] = function()
         require("lightspeed").setup({
+            exit_after_idle_msecs = { unlabeled = nil, labeled = nil },
             substitute_chars = {
                 [" "] = "␣",
                 ["\r"] = "¬",
