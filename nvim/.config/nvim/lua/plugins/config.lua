805b765dd6054ef500d22f25291562350d40cf9b
Peter Tri Ho <mail@petertriho.com>
2022-01-25 21:09:01 +1100

    fix(nvim): remove custom cmp comparator

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -366,8 +366,6 @@ return {
             end,
         })
 
-        local compare = require("cmp.config.compare")
-
         cmp.setup({
             documentation = {
                 border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
@@ -437,20 +435,6 @@ return {
                     },
                 },
                 { name = "vsnip" },
-            },
-            sorting = {
-                priority_weight = 2,
-                comparators = {
-                    require("cmp_tabnine.compare"),
-                    compare.offset,
-                    compare.exact,
-                    compare.score,
-                    compare.recently_used,
-                    compare.kind,
-                    compare.sort_text,
-                    compare.length,
-                    compare.order,
-                },
             },
         })
 
@@ -1081,7 +1065,7 @@ return {
         vim.api.nvim_set_keymap("n", "g]", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
     end,
     ["tzachar/cmp-tabnine"] = function()
-        require("cmp_tabnine.config"):setup({
+        require("cmp_tabnine.config").setup({
             max_lines = 1000,
             max_num_results = 10,
             sort = true,
