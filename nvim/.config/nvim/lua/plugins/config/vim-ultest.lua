7ff3ca95741a513916c192cf6237db0ef6bde333
Peter Tri Ho <mail@petertriho.com>
2022-02-03 14:14:24 +1100

    revert(nvim): disable lazy loading for ultest

--- nvim/.config/nvim/lua/plugins/config/vim-ultest.lua
+++ nvim/.config/nvim/lua/plugins/config/vim-ultest.lua
@@ -2,18 +2,6 @@ return {
     "rcarriga/vim-ultest",
     requires = { "vim-test/vim-test" },
     run = ":UpdateRemotePlugins",
-    cmd = {
-        "UltestAttach",
-        "UltestClear",
-        "UltestDebug",
-        "UltestLast",
-        "UltestNearest",
-        "UltestOutput",
-        "Ultest",
-        "UltestStop",
-        "UltestSummary",
-    },
-    opt = true,
     setup = function()
         vim.g.ultest_output_on_run = 0
         vim.g.ultest_output_on_line = 0
