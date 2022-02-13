1bc0a15ff8b73e9c91cbb1d2d290280a87f711a1
Peter Tri Ho <mail@petertriho.com>
2022-02-13 19:25:00 +1100

    fix: change nvim-ts-hint-textobject keymap to H

--- nvim/.config/nvim/lua/plugins/config/nvim-ts-hint-textobject.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-ts-hint-textobject.lua
@@ -4,7 +4,7 @@ return {
         local set_keymap = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
 
-        set_keymap("o", "t", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
-        set_keymap("v", "t", ":lua require('tsht').nodes()<CR>", opts)
+        set_keymap("o", "H", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
+        set_keymap("v", "H", ":lua require('tsht').nodes()<CR>", opts)
     end,
 }
