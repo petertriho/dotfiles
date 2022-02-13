efe31eef4e498626c91078c6367822fdcf72062c
Peter Tri Ho <mail@petertriho.com>
2022-02-13 18:08:50 +1100

    revert(nvim): use default highlights defined by nvim-scrollbar

--- nvim/.config/nvim/lua/plugins/config/nvim-scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-scrollbar.lua
@@ -4,16 +4,8 @@ return {
         local colors = require("colors")
 
         require("scrollbar").setup({
-            handle = {
-                color = colors.bg_highlight,
-            },
             marks = {
                 Search = { color = colors.orange },
-                Error = { color = colors.error },
-                Warn = { color = colors.warning },
-                Info = { color = colors.info },
-                Hint = { color = colors.hint },
-                Misc = { color = colors.purple },
             },
             excluded_filetypes = {
                 "prompt",
