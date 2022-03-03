84e16addb8f8f89f474b39be9cb52755f94afd31
Peter Tri Ho <mail@petertriho.com>
2022-03-04 10:49:48 +1100

    feat(nvim): add tabular for markdown

--- nvim/.config/nvim/lua/plugins/config/nvim-markdown.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-markdown.lua
@@ -1,7 +1,11 @@
 return {
     "ixru/nvim-markdown",
+    requires = {
+        "godlygeek/tabular",
+    },
     ft = "markdown",
     setup = function()
+        vim.g.vim_markdown_conceal = 0
         vim.g.vim_markdown_no_default_key_mappings = 1
     end,
 }
