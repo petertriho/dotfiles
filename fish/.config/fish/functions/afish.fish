28f330f62640c90b8859cf02b7120505d5468706
Peter Tri Ho <mail@petertriho.com>
2022-03-10 15:49:20 +1100

    feat(fish): add asdf nodejs version to arch variants

--- fish/.config/fish/functions/afish.fish
+++ fish/.config/fish/functions/afish.fish
@@ -1,5 +1,9 @@
 function afish --wraps fish --description "Fish (ARM)"
     arch -arm64 /opt/homebrew/bin/fish \
-        --init-command "set -e ASDF_DEFAULT_TOOL_VERSIONS_FILENAME; set -e ASDF_DATA_DIR; set -e ASDF_PYTHON_VERSION" \
+        --init-command "
+    set -e ASDF_DATA_DIR;
+    set -e ASDF_DEFAULT_TOOL_VERSIONS_FILENAME;
+    set -e ASDF_NODEJS_VERSION;
+    set -e ASDF_PYTHON_VERSION;" \
         $argv
 end
--- fish/.config/fish/functions/xfish.fish
+++ fish/.config/fish/functions/xfish.fish
@@ -1,7 +1,13 @@
 function xfish --wraps fish --description "Fish (x86)"
     # https://github.com/asdf-vm/asdf/issues/1082 issue with tool versions
+    set NODEJS_VERSION (grep "nodejs" "$HOME/.tool-versions-x86" | sed "s/nodejs *\(.*\)/\1/")
     set PYTHON_VERSION (grep "python" "$HOME/.tool-versions-x86" | sed "s/python *\(.*\)/\1/")
+
     arch -x86_64 /usr/local/bin/fish \
-        --init-command "set -x ASDF_DEFAULT_TOOL_VERSIONS_FILENAME $HOME/.tool-versions-x86; set -x ASDF_DATA_DIR $HOME/.asdf_x86; set -x ASDF_PYTHON_VERSION $PYTHON_VERSION" \
+        --init-command "
+    set -x ASDF_DATA_DIR $HOME/.asdf_x86; \
+    set -x ASDF_DEFAULT_TOOL_VERSIONS_FILENAME $HOME/.tool-versions-x86; \
+    set -x ASDF_NODEJS_VERSION $NODEJS_VERSION;
+    set -x ASDF_PYTHON_VERSION $PYTHON_VERSION;" \
         $argv
 end
