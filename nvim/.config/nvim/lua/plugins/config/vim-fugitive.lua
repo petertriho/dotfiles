7913a07aed616f6436f1e9ffec3db941ff2124da
Peter Tri Ho <mail@petertriho.com>
2022-03-11 12:46:56 +1100

    fix(nvim): change diffget keymaps to match unimpaired

--- nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
@@ -2,7 +2,7 @@ return {
     "tpope/vim-fugitive",
     cmd = { "G", "Git" },
     config = function()
-        vim.api.nvim_set_keymap("n", "g[", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
-        vim.api.nvim_set_keymap("n", "g]", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
+        vim.api.nvim_set_keymap("n", "[g", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
+        vim.api.nvim_set_keymap("n", "]g", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
     end,
 }
