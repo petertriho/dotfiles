52c23a54ff9ca07e3e4bdf907513aadb131cfd24
Peter Tri Ho <mail@petertriho.com>
2022-03-13 12:22:02 +1100

    fix(nvim): diffget keymaps moved to fugitive setup

--- nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive",
     cmd = { "G", "Git" },
-    config = function()
+    setup = function()
         vim.api.nvim_set_keymap("n", "[g", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
         vim.api.nvim_set_keymap("n", "]g", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
     end,
