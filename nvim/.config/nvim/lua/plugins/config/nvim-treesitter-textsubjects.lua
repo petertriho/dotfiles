c57f33bd9085117c20357aeca400d0e7c616db9d
Peter Tri Ho <mail@petertriho.com>
2022-03-14 14:23:17 +1100

    feat(nvim): register textsubjects to whichkey

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter-textsubjects.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter-textsubjects.lua
@@ -3,16 +3,20 @@ return {
     requires = "nvim-treesitter/nvim-treesitter",
     after = "nvim-treesitter",
     config = function()
+        local textobjects = {
+            ["."] = "textsubjects-smart",
+            ["<CR>"] = "textsubjects-container-outer",
+            ["i<CR>"] = "textsubjects-container-inner",
+        }
+
         require("nvim-treesitter.configs").setup({
             textsubjects = {
                 enable = true,
                 prev_selection = "<BS>",
-                keymaps = {
-                    ["."] = "textsubjects-smart",
-                    ["<CR>"] = "textsubjects-container-outer",
-                    ["i<CR>"] = "textsubjects-container-inner",
-                },
+                keymaps = textobjects,
             },
         })
+
+        require("which-key").register(textobjects, { mode = "o", prefix = "" })
     end,
 }
