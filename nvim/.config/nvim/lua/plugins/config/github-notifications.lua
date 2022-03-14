0775c7f1a76c7fe9c727b58e7ee4d68de87a484a
Peter Tri Ho <mail@petertriho.com>
2022-03-15 01:01:39 +1100

    feat(nvim): add mappings for github-notifications

--- nvim/.config/nvim/lua/plugins/config/github-notifications.lua
+++ nvim/.config/nvim/lua/plugins/config/github-notifications.lua
@@ -4,6 +4,20 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        require("github-notifications").setup()
+        require("github-notifications").setup({
+            cache = true,
+            mappings = {
+                hide = "h",
+                mark_all_read = "a",
+                mark_read = "r",
+                open_in_browser = "o",
+            },
+            prompt_mappings = {
+                hide = "<C-h>",
+                mark_all_read = "<C-a>",
+                mark_read = "<C-r>",
+                open_in_browser = "<C-o>",
+            },
+        })
     end,
 }
