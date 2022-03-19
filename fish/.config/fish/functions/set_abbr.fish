8ee50584e607498bce5ea0dddd94c12b2c59d2c8
Peter Tri Ho <mail@petertriho.com>
2022-03-19 13:46:03 +1100

    feat(fish): add gma (git merge --abort) abbreviation

--- fish/.config/fish/functions/set_abbr.fish
+++ fish/.config/fish/functions/set_abbr.fish
@@ -27,6 +27,7 @@ function set_abbr --description "Set abbr(eviations)"
     abbr -a -U -- gcob 'git checkout -b'
     abbr -a -U -- gf   'git fetch'
     abbr -a -U -- gm   'git merge'
+    abbr -a -U -- gma  'git merge --abort'
     abbr -a -U -- gmt  'git mergetool'
     abbr -a -U -- gpl  'git pull'
     abbr -a -U -- gplo 'git pull origin'
