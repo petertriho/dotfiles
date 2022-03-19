6e6becdbcc50e495501003e4fc4fe5e154ea0c2c
Peter Tri Ho <mail@petertriho.com>
2022-03-19 13:47:55 +1100

    feat: add fugitive as git mergetool

--- nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
+++ nvim/.config/nvim/lua/plugins/config/vim-fugitive.lua
@@ -1,6 +1,11 @@
 return {
     "tpope/vim-fugitive",
-    cmd = { "G", "Git" },
+    cmd = {
+        "G",
+        "Gdiffsplit",
+        "Git",
+        "Gvdiffsplit",
+    },
     setup = function()
         vim.api.nvim_set_keymap("n", "g[", "<CMD>diffget //2<CR>", { silent = true, noremap = true })
         vim.api.nvim_set_keymap("n", "g]", "<CMD>diffget //3<CR>", { silent = true, noremap = true })
