fca99e62f9702425edaf646b9aa853be40bbcb93
Peter Tri Ho <mail@petertriho.com>
2022-03-21 10:48:47 +1100

    fix(nvim): remove condensing multiple newlines into one

--- nvim/.config/nvim/lua/plugins/config/tidy.lua
+++ nvim/.config/nvim/lua/plugins/config/tidy.lua
@@ -2,7 +2,13 @@ return {
     "mcauley-penney/tidy.nvim",
     branch = "cfg",
     config = function()
-        require("tidy").setup({})
+        require("tidy").setup({
+            fmts = {
+                "eof",
+                "sof",
+                "ws",
+            },
+        })
     end,
     event = "BufWritePre",
 }
