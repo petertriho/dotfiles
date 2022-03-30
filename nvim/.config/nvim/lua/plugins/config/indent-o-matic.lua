137171b36bb079514c87c08108faacc832246a26
Peter Tri Ho <mail@petertriho.com>
2022-03-30 17:03:08 +1100

    perf(nvim): reduce contraints for indent-o-matic

--- nvim/.config/nvim/lua/plugins/config/indent-o-matic.lua
+++ nvim/.config/nvim/lua/plugins/config/indent-o-matic.lua
@@ -1,6 +1,9 @@
 return {
     "Darazaki/indent-o-matic",
     config = function()
-        require("indent-o-matic").setup({})
+        require("indent-o-matic").setup({
+            max_lines = 128,
+            standard_widths = { 2, 4 },
+        })
     end,
 }
