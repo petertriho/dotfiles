44514415a015a837a4d67fb5ad4916f4d9bfd42b
Peter Tri Ho <mail@petertriho.com>
2022-03-30 13:03:51 +1100

    fix(fish): increase threshold to show side-by-side for delta

--- fish/.config/fish/functions/delta_side_by_side.fish
+++ fish/.config/fish/functions/delta_side_by_side.fish
@@ -1,7 +1,7 @@
 function delta_side_by_side --on-signal WINCH
-    if test "$COLUMNS" -ge 120; and ! contains side-by-side "$DELTA_FEATURES"
+    if test "$COLUMNS" -ge 180; and ! contains side-by-side "$DELTA_FEATURES"
         set --global --export --append DELTA_FEATURES side-by-side
-    else if test "$COLUMNS" -lt 120; and contains side-by-side "$DELTA_FEATURES"
+    else if test "$COLUMNS" -lt 180; and contains side-by-side "$DELTA_FEATURES"
         set --erase DELTA_FEATURES[(contains --index side-by-side "$DELTA_FEATURES")]
     end
 end
