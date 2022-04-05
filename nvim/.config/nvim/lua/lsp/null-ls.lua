865588805fbf2608f2aa2053d91a008f40075e12
Peter Tri Ho <mail@petertriho.com>
2022-04-05 22:17:04 +1000

    feat: add reorder-python-imports

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -277,6 +277,25 @@ M.setup = function(overrides)
                     return extra_args
                 end,
             }),
+            b.formatting.reorder_python_imports.with({
+                extra_args = function(params)
+                    local extra_args = {}
+
+                    local python_version = vim.fn.system("python --version")
+
+                    if python_version >= "3.9" then
+                        table.insert(extra_args, "--py39-plus")
+                    elseif python_version >= "3.7" then
+                        table.insert(extra_args, "--py37-plus")
+                    elseif python_version >= "3.6" then
+                        table.insert(extra_args, "--py36-plus")
+                    elseif python_version >= "3" then
+                        table.insert(extra_args, "--py3-plus")
+                    end
+
+                    return extra_args
+                end,
+            }),
             sources_formatting.black.with({
                 condition = function(utils)
                     return not check_if_black_accepts_stdin()
