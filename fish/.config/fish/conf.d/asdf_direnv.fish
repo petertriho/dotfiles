8858d9b819ab79720e6858d23e4dbeac8f0ac1ad
Peter Tri Ho <mail@petertriho.com>
2022-04-06 20:43:59 +1000

    fix(fish): remove unnecesary conf.d asdf_direnv

--- fish/.config/fish/conf.d/asdf_direnv.fish
+++
@@ -1,4 +0,0 @@
-set CURR_DIRENV_VERSION (grep "direnv" "$HOME/.tool-versions" | sed "s/direnv *\(.*\)/\1/")
-
-set -gx ASDF_DIRENV_BIN "$HOME/.asdf/installs/direnv/$CURR_DIRENV_VERSION/bin/direnv"
-test -e $ASDF_DIRENV_BIN; and $ASDF_DIRENV_BIN hook fish | source
