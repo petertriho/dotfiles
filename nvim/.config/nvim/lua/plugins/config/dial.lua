ee40b4fb7d91983328a29634d9ccaf9afa874dbd
Peter Tri Ho <mail@petertriho.com>
2022-04-10 11:45:06 +1000

    fix(nvim): dial keymaps

--- nvim/.config/nvim/lua/plugins/config/dial.lua
+++ nvim/.config/nvim/lua/plugins/config/dial.lua
@@ -3,15 +3,13 @@ return {
     keys = {
         "<Plug>(dial-increment)",
         "<Plug>(dial-decrement)",
-        "<Plug>(dial-increment-additional)",
-        "<Plug>(dial-decrement-additional)",
     },
     setup = function()
         local set_keymap = vim.api.nvim_set_keymap
 
         set_keymap("", "<C-a>", "<Plug>(dial-increment)", {})
         set_keymap("", "<C-x>", "<Plug>(dial-decrement)", {})
-        set_keymap("v", "g<C-a>", "<Plug>(dial-increment-additional)", {})
-        set_keymap("v", "g<C-x>", "<Plug>(dial-decrement-additional)", {})
+        set_keymap("v", "g<C-a>", "g<Plug>(dial-increment)", {})
+        set_keymap("v", "g<C-x>", "g<Plug>(dial-decrement)", {})
     end,
 }
