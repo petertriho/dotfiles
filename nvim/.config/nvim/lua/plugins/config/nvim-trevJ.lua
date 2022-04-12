e57a0a4338a63c6cd80015a8eac9e0bda6fd8575
Peter Tri Ho <mail@petertriho.com>
2022-04-12 15:31:54 +1000

    fix(nvim): duplicate keymap to `gJ`

--- nvim/.config/nvim/lua/plugins/config/nvim-trevJ.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-trevJ.lua
@@ -3,7 +3,7 @@ return {
     module = "trevj",
     after = "nvim-treesitter",
     setup = function()
-        vim.keymap.set("n", "gJ", function()
+        vim.keymap.set("n", "<Leader>J", function()
             require("trevj").format_at_cursor()
         end, {})
     end,
