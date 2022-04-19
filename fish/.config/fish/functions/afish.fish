81692aba1ab6cd5459ebe5ee74095c22164a1fbd
Peter Tri Ho <mail@petertriho.com>
2022-04-19 13:54:20 +1000

    refactor(fish): switch out fish env variables to arch ones

--- fish/.config/fish/functions/afish.fish
+++ fish/.config/fish/functions/afish.fish
@@ -1,14 +1,13 @@
 function afish --wraps fish --description "Fish (ARM)"
-    arch -arm64 /opt/homebrew/bin/fish \
-        --init-command '
-            fish_add_path -amP $ASDF_DATA_DIR/shims;
-            set -e ASDF_DATA_DIR;
-            set -e ASDF_DEFAULT_TOOL_VERSIONS_FILENAME;
-            set -e ASDF_NODEJS_VERSION;
-            set -e ASDF_NPM_DEFAULT_PACKAGES_FILE;
-            set -e ASDF_PYTHON_DEFAULT_PACKAGES_FILE;
-            set -e ASDF_PYTHON_VERSION;
-            set -e DOCKER_DEFAULT_PLATFORM;
-        ' \
+    arch -arm64 \
+        -d ASDF_DATA_DIR \
+        -d ASDF_DEFAULT_TOOL_VERSIONS_FILENAME \
+        -d ASDF_NODEJS_VERSION \
+        -d ASDF_NPM_DEFAULT_PACKAGES_FILE \
+        -d ASDF_PYTHON_DEFAULT_PACKAGES_FILE \
+        -d ASDF_PYTHON_VERSION \
+        -d DOCKER_DEFAULT_PLATFORM \
+        -d PATH \
+        /opt/homebrew/bin/fish \
         $argv
 end
--- fish/.config/fish/functions/xfish.fish
+++ fish/.config/fish/functions/xfish.fish
@@ -7,12 +7,11 @@ function xfish --wraps fish --description "Fish (x86)"
         set -x ASDF_PYTHON_VERSION (grep "python" "$HOME/.tool-versions-x86" | sed "s/python *\(.*\)/\1/")
     end
 
-    set -x ASDF_DATA_DIR "$HOME/.asdf_x86"
-    set -x ASDF_DEFAULT_TOOL_VERSIONS_FILENAME "$HOME/.tool-versions-x86"
-    set -x ASDF_NPM_DEFAULT_PACKAGES_FILE "$HOME/.default-npm-packages-x86"
-    set -x ASDF_PYTHON_DEFAULT_PACKAGES_FILE "$HOME/.default-python-packages-x86"
-
-    set -x DOCKER_DEFAULT_PLATFORM linux/amd64
-
-    arch -x86_64 /usr/local/bin/fish $argv
+    arch -x86_64 \
+        -e ASDF_DATA_DIR="$HOME/.asdf_x86" \
+        -e ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$HOME/.tool-versions-x86" \
+        -e ASDF_NPM_DEFAULT_PACKAGES_FILE="$HOME/.default-npm-packages-x86" \
+        -e ASDF_PYTHON_DEFAULT_PACKAGES_FILE="$HOME/.default-python-packages-x86" \
+        -e DOCKER_DEFAULT_PLATFORM=linux/amd64 \
+        /usr/local/bin/fish $argv
 end
