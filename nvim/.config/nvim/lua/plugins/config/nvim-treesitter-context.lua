314c56a8fc402c6a802907a249b1b5e81ff81b2a
Peter Tri Ho <mail@petertriho.com>
2022-04-21 09:22:47 +1000

    fix(nvim): switch to forked version of nvim-treesitter-context

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter-context.lua
@@ -1,5 +1,5 @@
 return {
-    "romgrk/nvim-treesitter-context",
+    "lewis6991/nvim-treesitter-context",
     requires = "nvim-treesitter/nvim-treesitter",
     after = "nvim-treesitter",
     config = function()
