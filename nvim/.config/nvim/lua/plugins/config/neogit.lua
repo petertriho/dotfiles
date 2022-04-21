53264d71bdc69b3d928e73de039e192db4270402
Peter Tri Ho <mail@petertriho.com>
2022-04-21 12:09:05 +1000

    feat(nvim): add toggle neogit tab

--- nvim/.config/nvim/lua/plugins/config/neogit.lua
+++ nvim/.config/nvim/lua/plugins/config/neogit.lua
@@ -3,6 +3,26 @@ return {
     requires = "nvim-lua/plenary.nvim",
     cmd = "Neogit",
     module = "neogit",
+    setup = function()
+        vim.api.nvim_create_user_command("ToggleNeogitStatusTab", function()
+            local curr_bufname = vim.fn.bufname("NeogitStatus")
+
+            if vim.fn.buflisted(curr_bufname) == 1 then
+                local curr_bufnr = vim.fn.bufnr("NeogitStatus")
+
+                for tabnr = 1, vim.fn.tabpagenr("$") do
+                    for _, bufnr in ipairs(vim.fn.tabpagebuflist(tabnr)) do
+                        if bufnr == curr_bufnr then
+                            vim.cmd("tabclose")
+                            return
+                        end
+                    end
+                end
+            end
+
+            vim.cmd("Neogit")
+        end, {})
+    end,
     config = function()
         require("neogit").setup({
             disable_commit_confirmation = true,
