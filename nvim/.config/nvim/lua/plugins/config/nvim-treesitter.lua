5b3a3bb563d05b49696062b2a12ad28d54f204f1
Peter Tri Ho <mail@petertriho.com>
2022-04-27 16:13:54 +1000

    fix(nvim): `get_node_text` errors

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -4,7 +4,29 @@ return {
     run = ":TSUpdateSync",
     config = function()
         local ts_utils = require("nvim-treesitter.ts_utils")
-        ts_utils.get_node_text = vim.treesitter.query.get_node_text
+        ts_utils.get_node_text = function(node, bufnr)
+            bufnr = bufnr or vim.api.nvim_get_current_buf()
+            if not node then
+                return {}
+            end
+
+            -- We have to remember that end_col is end-exclusive
+            local start_row, start_col, end_row, end_col = ts_utils.get_node_range(node)
+
+            if start_row ~= end_row then
+                local lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row + 1, false)
+                lines[1] = string.sub(lines[1], start_col + 1)
+                -- end_row might be just after the last line. In this case the last line is not truncated.
+                if #lines == end_row - start_row + 1 then
+                    lines[#lines] = string.sub(lines[#lines], 1, end_col)
+                end
+                return lines
+            else
+                local line = vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, false)[1]
+                -- If line is nil then the line is empty
+                return line and { string.sub(line, start_col + 1, end_col) } or {}
+            end
+        end
 
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
