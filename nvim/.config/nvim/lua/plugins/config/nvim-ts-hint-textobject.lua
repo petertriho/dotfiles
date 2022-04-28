1f942d78fe0fe22a67c52ef0ccd6caa7f0578a4a
Peter Tri Ho <mail@petertriho.com>
2022-04-28 10:15:30 +1000

    feat(nvim): change `nvim-ts-hint-textobject` keymap to `m`

--- nvim/.config/nvim/lua/plugins/config/nvim-ts-hint-textobject.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-ts-hint-textobject.lua
@@ -4,7 +4,7 @@ return {
         local set_keymap = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
 
-        set_keymap("o", "H", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
-        set_keymap("v", "H", ":lua require('tsht').nodes()<CR>", opts)
+        set_keymap("o", "m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
+        set_keymap("v", "m", ":lua require('tsht').nodes()<CR>", opts)
     end,
 }
