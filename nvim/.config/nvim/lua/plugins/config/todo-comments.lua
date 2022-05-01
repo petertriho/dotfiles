bc921ba2bd446fbd8c67f23c342ca42fc1cd4ec7
Peter Tri Ho <mail@petertriho.com>
2022-05-01 23:40:44 +1000

    fix(nvim): `todo-comments` causing command-line window errors

--- nvim/.config/nvim/lua/plugins/config/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/config/todo-comments.lua
@@ -4,5 +4,19 @@ return {
     opt = true,
     config = function()
         require("todo-comments").setup()
+
+        local Config = require("todo-comments.config")
+        local highlight = require("todo-comments.highlight")
+        highlight.is_valid_buf = function(buf)
+            local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
+            if buftype ~= "" or buftype ~= "nofile" or buftype ~= "quickfix" then
+                return false
+            end
+            local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
+            if vim.tbl_contains(Config.options.highlight.exclude, filetype) then
+                return false
+            end
+            return true
+        end
     end,
 }
