d93bb4a50f9b5dba22253ed0687964b5a84aa1c6
Peter Tri Ho <mail@petertriho.com>
2022-05-10 10:47:52 +1000

    revert(nvim): restore `TSServerOrganizeImports` command until lspconfig is updated

--- nvim/.config/nvim/lua/lsp/config.lua
+++ nvim/.config/nvim/lua/lsp/config.lua
@@ -68,6 +68,18 @@ return {
     terraformls = {},
     tflint = {},
     tsserver = {
+        commands = {
+            TSServerOrganizeImports = {
+                function()
+                    local params = {
+                        command = "_typescript.organizeImports",
+                        arguments = { vim.api.nvim_buf_get_name(0) },
+                    }
+                    vim.lsp.buf.execute_command(params)
+                end,
+                description = "Organize Imports",
+            },
+        },
         user_commands = {
             {
                 name = "TSServerOrganizeImports",
