653ba1266edf9ed421a88b1ca34b39d1b14058b3
Peter Tri Ho <mail@petertriho.com>
2022-05-17 11:57:25 +1000

    fix(nvim): remove `repos` from `github-notifications` url

--- nvim/.config/nvim/lua/plugins/config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/config/telescope.lua
@@ -70,7 +70,7 @@ return {
 
         local commands = require("github-notifications.telescope.commands")
         commands.open_in_browser = function(notification)
-            local url = string.gsub(notification.value.subject.url, "api%.github%.com", "github.com"):gsub(
+            local url = string.gsub(notification.value.subject.url, "api%.github%.com/repos", "github.com"):gsub(
                 "/pulls/(%d+)",
                 "/pull/%1"
             )
