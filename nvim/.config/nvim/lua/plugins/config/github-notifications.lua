2cddab90a96dfaf6e71a7ab2c2826f608e0affcf
Peter Tri Ho <mail@petertriho.com>
2022-05-17 10:47:54 +1000

    fix(nvim): telescope not loading

--- nvim/.config/nvim/lua/plugins/config/github-notifications.lua
+++ nvim/.config/nvim/lua/plugins/config/github-notifications.lua
@@ -4,21 +4,6 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        local commands = require("github-notifications.telescope.commands")
-        commands.open_in_browser = function(notification)
-            local url = notification.value.subject.url
-
-            url = url:gsub("api%.github%.com", "github.com")
-
-            if vim.fn.has("mac") == 1 then
-                vim.cmd('call jobstart(["open", expand("' .. url .. '")], {"detach": v:true})')
-            elseif vim.fn.has("unix") == 1 then
-                vim.cmd('call jobstart(["xdg-open", expand("' .. url .. '")], {"detach": v:true})')
-            else
-                vim.notify("gx not supported on this OS", vim.log.levels.ERROR)
-            end
-        end
-
         require("github-notifications").setup({
             mappings = {
                 hide = "h",
--- nvim/.config/nvim/lua/plugins/config/telescope.lua
+++ nvim/.config/nvim/lua/plugins/config/telescope.lua
@@ -67,5 +67,20 @@ return {
         })
 
         telescope.load_extension("fzf")
+
+        local commands = require("github-notifications.telescope.commands")
+        commands.open_in_browser = function(notification)
+            local url = notification.value.subject.url
+
+            url = url:gsub("api%.github%.com", "github.com")
+
+            if vim.fn.has("mac") == 1 then
+                vim.cmd('call jobstart(["open", expand("' .. url .. '")], {"detach": v:true})')
+            elseif vim.fn.has("unix") == 1 then
+                vim.cmd('call jobstart(["xdg-open", expand("' .. url .. '")], {"detach": v:true})')
+            else
+                vim.notify("gx not supported on this OS", vim.log.levels.ERROR)
+            end
+        end
     end,
 }
