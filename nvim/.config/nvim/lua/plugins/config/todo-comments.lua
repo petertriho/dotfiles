530aac555ba331b1cd4b81a283361d2be6ffa45d
Peter Tri Ho <mail@petertriho.com>
2022-05-17 16:52:17 +1000

    fix(nvim): `todo-comments` highlights not showing

--- nvim/.config/nvim/lua/plugins/config/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/config/todo-comments.lua
@@ -5,15 +5,11 @@ return {
     config = function()
         require("todo-comments").setup()
 
-        local Config = require("todo-comments.config")
         local highlight = require("todo-comments.highlight")
         highlight.is_valid_buf = function(buf)
             local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
-            if buftype ~= "" or buftype ~= "nofile" or buftype ~= "quickfix" then
-                return false
-            end
             local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
-            if vim.tbl_contains(Config.options.highlight.exclude, filetype) then
+            if (buftype == "" and filetype == "") or buftype == "nofile" or buftype == "quickfix" then
                 return false
             end
             return true
