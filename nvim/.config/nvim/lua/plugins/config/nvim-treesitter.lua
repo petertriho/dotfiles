2c4e48a8e63b42af69997748bd5d6e43af3b2bfb
Peter Tri Ho <mail@petertriho.com>
2022-06-29 09:06:21 +1000

    feat(nvim): add `markdown_inline` to treesitter

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -7,6 +7,7 @@ return {
             ensure_installed = {
                 "bash",
                 "comment",
+                "markdown_inline",
                 "prisma",
                 "regex",
                 "todotxt",
