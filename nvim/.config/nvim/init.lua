90c375c9f350e7460cc05314c7093ab7a9629797
Peter Tri Ho <mail@petertriho.com>
2022-07-14 18:46:00 +1000

    fix(nvim): restore `impatient`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,7 @@
 vim.opt.shadafile = "NONE"
+if vim.fn.isdirectory(vim.fn.stdpath("data") .. "/site/pack/packer/start/impatient.nvim") == 1 then
+    require("impatient")
+end
 require("options")
 require("plugins")
 require("lsp")
