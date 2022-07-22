d1e997d6144d0ef84234a4b51eac5c313a5599aa
Peter Tri Ho <mail@petertriho.com>
2022-07-22 20:01:26 +1000

    fix(nvim): rename `nvim-ts-hint-textobject` -> `nvim-treehopper`, fix visual keymap

--- nvim/.config/nvim/lua/plugins/config/nvim-treehopper.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treehopper.lua
@@ -1,10 +1,10 @@
 return {
-    "mfussenegger/nvim-ts-hint-textobject",
+    "mfussenegger/nvim-treehopper",
     setup = function()
         local set_keymap = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
 
         set_keymap("o", "m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
-        set_keymap("v", "m", ":lua require('tsht').nodes()<CR>", opts)
+        set_keymap("x", "m", ":lua require('tsht').nodes()<CR>", opts)
     end,
 }
