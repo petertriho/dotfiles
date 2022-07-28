8e02e0aa43831919a3164db42bd67498dc96ecaf
Peter Tri Ho <mail@petertriho.com>
2022-07-29 09:26:23 +1000

    feat(nvim): add `yanky` keymaps

--- nvim/.config/nvim/lua/plugins/config/yanky.lua
+++ nvim/.config/nvim/lua/plugins/config/yanky.lua
@@ -7,7 +7,7 @@ return {
     setup = function()
         local set_keymap = vim.keymap.set
 
-        set_keymap({ "n", "x" }, "y", "<Plug>(YankyYank)", {})
+        set_keymap({ "n", "x" }, "y", "<Plug>(YankyYank)")
     end,
     config = function()
         require("yanky").setup({
@@ -23,14 +23,23 @@ return {
 
         local set_keymap = vim.keymap.set
 
-        set_keymap({ "n", "x" }, "[p", "<Plug>(YankyGPutBefore)", {})
-        set_keymap({ "n", "x" }, "]p", "<Plug>(YankyGPutAfter)", {})
-        set_keymap({ "n", "x" }, "p", "<Plug>(YankyPutAfter)", {})
-        set_keymap({ "n", "x" }, "P", "<Plug>(YankyPutBefore)", {})
+        set_keymap({ "n", "x" }, "[p", "<Plug>(YankyGPutBefore)")
+        set_keymap({ "n", "x" }, "]p", "<Plug>(YankyGPutAfter)")
+        set_keymap({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+        set_keymap({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
 
-        set_keymap("n", "<M-f>", "<Plug>(YankyCycleForward)", {})
-        set_keymap("n", "<M-b>", "<Plug>(YankyCycleBackward)", {})
+        set_keymap("n", "]P", "<Plug>(YankyPutIndentAfterLinewise)")
+        set_keymap("n", "[P", "<Plug>(YankyPutIndentBeforeLinewise)")
+        set_keymap("n", ">p", "<Plug>(YankyPutIndentAfterShiftRight)")
+        set_keymap("n", "<p", "<Plug>(YankyPutIndentAfterShiftLeft)")
+        set_keymap("n", ">P", "<Plug>(YankyPutIndentBeforeShiftRight)")
+        set_keymap("n", "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)")
+        set_keymap("n", "=p", "<Plug>(YankyPutAfterFilter)")
+        set_keymap("n", "=P", "<Plug>(YankyPutBeforeFilter)")
 
-        set_keymap("n", "Y", "y$", {})
+        set_keymap("n", "<M-f>", "<Plug>(YankyCycleForward)")
+        set_keymap("n", "<M-b>", "<Plug>(YankyCycleBackward)")
+
+        set_keymap("n", "Y", "y$")
     end,
 }
