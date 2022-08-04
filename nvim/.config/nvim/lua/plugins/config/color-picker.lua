419c187d26eb74b01fabbdce0ed7ab28bf08b748
Peter Tri Ho <mail@petertriho.com>
2022-08-04 16:54:50 +1000

    refactor(nvim): change color picker keymap to `<M-c>`

--- nvim/.config/nvim/lua/plugins/config/color-picker.lua
+++ nvim/.config/nvim/lua/plugins/config/color-picker.lua
@@ -8,8 +8,8 @@ return {
         local set_keymap = vim.keymap.set
         local opts = { noremap = true, silent = true }
 
-        set_keymap("n", "<C-c>", "<CMD>PickColor<CR>", opts)
-        set_keymap("i", "<C-c>", "<CMD>PickColorInsert<CR>", opts)
+        set_keymap("n", "<M-c>", "<CMD>PickColor<CR>", opts)
+        set_keymap("i", "<M-c>", "<CMD>PickColorInsert<CR>", opts)
     end,
     config = function()
         require("color-picker").setup()
