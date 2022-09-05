185cdcc2bd6bf0b232c02025e802e5b6b60e7c15
Peter Tri Ho <mail@petertriho.com>
2022-09-06 09:22:26 +1000

    revert(nvim): remove unnecessary fix for `todo-comments`

--- nvim/.config/nvim/lua/plugins/config/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/config/todo-comments.lua
@@ -4,31 +4,5 @@ return {
     opt = true,
     config = function()
         require("todo-comments").setup()
-
-        local highlight = require("todo-comments.highlight")
-        highlight.is_valid_buf = function(buf)
-            local buftype = vim.api.nvim_buf_get_option(buf, "buftype")
-            local filetype = vim.api.nvim_buf_get_option(buf, "filetype")
-            if (buftype == "" and filetype == "") or buftype == "nofile" or buftype == "quickfix" then
-                return false
-            end
-            return true
-        end
-
-        highlight.is_valid_win = function(win)
-            if not vim.api.nvim_win_is_valid(win) then
-                return false
-            end
-            -- avoid E5108 after pressing q:
-            if vim.fn.getcmdwintype() ~= "" then
-                return false
-            end
-            -- dont do anything for floating windows
-            if highlight.is_float(win) then
-                return false
-            end
-            local buf = vim.api.nvim_win_get_buf(win)
-            return highlight.is_valid_buf(buf)
-        end
     end,
 }
