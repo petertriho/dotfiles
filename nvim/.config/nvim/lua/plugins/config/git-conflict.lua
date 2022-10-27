983057060f3601efbf34fb3c99d853df203a0bab
Peter Tri Ho <mail@petertriho.com>
2022-10-27 11:57:56 +1100

    revert(nvim): back to `v1.0.0` for `git-conflict`

--- nvim/.config/nvim/lua/plugins/config/git-conflict.lua
+++ nvim/.config/nvim/lua/plugins/config/git-conflict.lua
@@ -1,6 +1,7 @@
 return {
     "akinsho/git-conflict.nvim",
     after = "tokyonight.nvim",
+    tag = "v1.0.0",
     config = function()
         require("git-conflict").setup({
             highlights = {
