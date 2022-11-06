de7e8d18e436ee20a633cbe0eb315227bed478d5
Peter Tri Ho <mail@petertriho.com>
2022-11-06 01:32:45 +0000

    fix(nvim): restore nvim visual "x/X" keymap

--- nvim/.config/nvim/lua/plugins/config/leap.lua
+++ nvim/.config/nvim/lua/plugins/config/leap.lua
@@ -2,5 +2,9 @@ return {
     "ggandor/leap.nvim",
     config = function()
         require("leap").add_default_mappings()
+        vim.keymap.del({ "x", "o" }, "x")
+        vim.keymap.del({ "x", "o" }, "X")
+        vim.keymap.set({ "x", "o" }, "z", "<Plug>(leap-forward-till)")
+        vim.keymap.set({ "x", "o" }, "Z", "<Plug>(leap-backward-till)")
     end,
 }
