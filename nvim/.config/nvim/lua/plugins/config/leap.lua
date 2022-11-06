608f2822e1b9bf70f15cb606d0cce2205faf554c
Peter Tri Ho <mail@petertriho.com>
2022-11-06 02:18:21 +0000

    refactor(nvim): move `leap` z keymaps to `nvim-treehopper`

--- nvim/.config/nvim/lua/plugins/config/leap.lua
+++ nvim/.config/nvim/lua/plugins/config/leap.lua
@@ -4,7 +4,5 @@ return {
         require("leap").add_default_mappings()
         vim.keymap.del({ "x", "o" }, "x")
         vim.keymap.del({ "x", "o" }, "X")
-        vim.keymap.set({ "x", "o" }, "z", "<Plug>(leap-forward-till)")
-        vim.keymap.set({ "x", "o" }, "Z", "<Plug>(leap-backward-till)")
     end,
 }
--- nvim/.config/nvim/lua/plugins/config/nvim-treehopper.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treehopper.lua
@@ -4,7 +4,7 @@ return {
         local set_keymap = vim.api.nvim_set_keymap
         local opts = { noremap = true, silent = true }
 
-        set_keymap("o", "m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
-        set_keymap("x", "m", ":lua require('tsht').nodes()<CR>", opts)
+        set_keymap("o", "z", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
+        set_keymap("x", "z", ":lua require('tsht').nodes()<CR>", opts)
     end,
 }
