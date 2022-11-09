3b99a866624599336eef00496d01cf13dff17ebe
Peter Tri Ho <mail@petertriho.com>
2022-11-09 13:29:43 +1100

    revert(nvim): disable twilight on startup in zen-mode

--- nvim/.config/nvim/lua/plugins/config/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/config/zen-mode.lua
@@ -2,6 +2,10 @@ return {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
     config = function()
-        require("zen-mode").setup()
+        require("zen-mode").setup({
+            plugins = {
+                twilight = { enabled = false },
+            },
+        })
     end,
 }
