4556d3f6c912b373706430dd6b40669bcf1105dd
Peter Tri Ho <mail@petertriho.com>
2022-11-11 11:25:49 +1100

    fix(nvim): disable `nvim-treesitter` for large files

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -18,6 +18,17 @@ return {
             highlight = {
                 enable = true,
                 use_languagetree = true,
+                disable = function(lang, bufnr)
+                    if vim.api.nvim_buf_line_count(bufnr) > 5000 then
+                        return true
+                    end
+
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
             },
             incremental_selection = {
                 enable = false,
