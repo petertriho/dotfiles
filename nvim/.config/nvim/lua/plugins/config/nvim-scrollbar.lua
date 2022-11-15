67a8183caa5b1f1ae372bc638cf6ab8c55dc6903
Peter Tri Ho <mail@petertriho.com>
2022-11-15 09:59:59 +0000

    revert(nvim): remove cursor handler in `nvim-scrollbar`

--- nvim/.config/nvim/lua/plugins/config/nvim-scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-scrollbar.lua
@@ -6,27 +6,12 @@ return {
         require("scrollbar").setup({
             folds = false,
             marks = {
-                Cursor = { text = "", priority = -1, highlight = "Normal" },
                 Search = { color = colors.orange },
                 GitAdd = { text = "│" },
                 GitChange = { text = "│" },
                 GitDelete = { text = "│" },
             },
             excluded_filetypes = require("filetypes").excludes,
-        })
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "CursorMoved", "CursorMovedI" }, {
-            callback = function()
-                local current_line = vim.fn.line(".") - 1
-                local bufnr = vim.fn.bufnr("%")
-
-                local utils = require("scrollbar.utils")
-                local scrollbar_marks = utils.get_scrollbar_marks(bufnr)
-                scrollbar_marks.cursor = { { line = current_line, text = "", type = "Cursor" } }
-                utils.set_scrollbar_marks(bufnr, scrollbar_marks)
-
-                require("scrollbar").render()
-            end,
-            desc = "Render scrollbar cursor",
         })
     end,
 }
