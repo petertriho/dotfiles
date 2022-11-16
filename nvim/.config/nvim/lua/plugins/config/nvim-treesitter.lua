e19258b762bb04a9cb335da89fc0f9718ae0e918
Peter Tri Ho <mail@petertriho.com>
2022-11-16 09:44:53 +0000

    revert(nvim): disable ts-comment

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -6,7 +6,7 @@ return {
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
                 "bash",
-                "comment",
+                -- "comment",
                 "markdown_inline",
                 "prisma",
                 "regex",
