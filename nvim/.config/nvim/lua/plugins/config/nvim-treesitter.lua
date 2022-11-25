5dc81759e864095c0ee9e18026dcc72670ed1086
Peter Tri Ho <mail@petertriho.com>
2022-11-25 15:04:19 +1100

    fix(nvim): disable `vim-illuminate` when `nvim-treesitter` is disabled

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -20,12 +20,14 @@ return {
                 use_languagetree = true,
                 disable = function(lang, bufnr)
                     if vim.api.nvim_buf_line_count(bufnr) > 5000 then
+                        pcall(require("illuminate").pause_buf)
                         return true
                     end
 
                     local max_filesize = 100 * 1024 -- 100 KB
                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
                     if ok and stats and stats.size > max_filesize then
+                        pcall(require("illuminate").pause_buf)
                         return true
                     end
                 end,
