c593e2e91492a1e68ef78c4723f5c8dea7ea3f16
Peter Tri Ho <mail@petertriho.com>
2022-11-25 12:35:40 +1100

    fix(nvim): glance keymaps clashing with default mappings

--- nvim/.config/nvim/lua/plugins/config/glance.lua
+++ nvim/.config/nvim/lua/plugins/config/glance.lua
@@ -14,15 +14,25 @@ return {
     config = function()
         local glance = require("glance")
         local actions = glance.actions
+
         glance.setup({
             height = 36,
             mappings = {
                 list = {
+                    ["\\"] = actions.jump_vsplit,
+                    ["-"] = actions.jump_split,
+                    ["s"] = "<Plug>(leap-forward-to)",
+                    ["v"] = "v",
                     ["gP"] = actions.enter_win("preview"),
+                    ["<leader>l"] = "<leader>l",
                 },
                 preview = {
                     ["gP"] = actions.enter_win("list"),
+                    ["<leader>l"] = "<leader>l",
                 },
+            },
+            folds = {
+                folded = false,
             },
         })
     end,
