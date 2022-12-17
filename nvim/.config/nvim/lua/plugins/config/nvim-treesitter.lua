2b28100f60e889c3a5ad00bd82064ef6e32af45a
Peter Tri Ho <mail@petertriho.com>
2022-12-17 12:24:59 +0000

    feat(nvim): add `help` treesitter parser

--- nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -7,6 +7,7 @@ return {
             ensure_installed = {
                 "bash",
                 -- "comment",
+                "help",
                 "markdown_inline",
                 "prisma",
                 "regex",
