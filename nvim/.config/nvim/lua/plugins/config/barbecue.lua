a03b33d97040b653f599d0f043c40f1900bf0f58
Peter Tri Ho <mail@petertriho.com>
2022-12-22 12:22:19 +0000

    perf(nvim): reduce number of calls to `barbecue` update

--- nvim/.config/nvim/lua/plugins/config/barbecue.lua
+++ nvim/.config/nvim/lua/plugins/config/barbecue.lua
@@ -5,9 +5,23 @@ return {
     },
     config = function()
         require("barbecue").setup({
+            create_autocmd = false,
+            attach_navic = false,
             modifiers = {
                 dirname = ":s?.*??",
             },
+        })
+
+        vim.api.nvim_create_autocmd({
+            "WinScrolled",
+            "BufWinEnter",
+            "CursorHold",
+            "InsertLeave",
+        }, {
+            group = vim.api.nvim_create_augroup("barbecue", {}),
+            callback = function()
+                require("barbecue.ui").update()
+            end,
         })
     end,
 }
