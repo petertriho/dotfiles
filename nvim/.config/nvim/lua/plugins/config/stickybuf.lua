5fc7472663ff71f2c8897662e7c17c1f54faa1b5
Peter Tri Ho <mail@petertriho.com>
2023-03-07 10:19:05 +1100

    fix: update `stickybuf` setup

--- nvim/.config/nvim/lua/plugins/config/stickybuf.lua
+++ nvim/.config/nvim/lua/plugins/config/stickybuf.lua
@@ -1,16 +1,18 @@
 return {
     "stevearc/stickybuf.nvim",
     config = function()
-        local sidebars = {}
-
-        for _, v in ipairs(require("filetypes").sidebars) do
-            if v ~= "NvimTree" then
-                sidebars[v] = "filetype"
-            end
-        end
-
         require("stickybuf").setup({
-            filetype = sidebars,
+            get_auto_pin = function(bufnr)
+                local filetype = vim.bo[bufnr].filetype
+
+                for _, v in ipairs(require("filetypes").sidebars) do
+                    if v ~= "NvimTree" and v == filetype then
+                        return "filetype"
+                    end
+                end
+
+                return require("stickybuf").should_auto_pin(bufnr)
+            end,
         })
     end,
 }
