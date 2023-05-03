0a518eae35ae432df4dd90036bdd112097a556b3
Peter Tri Ho <mail@petertriho.com>
2023-05-03 20:55:23 +1000

    feat(bootstrap): add start11

--- bootstrap/windows/bootstrap.ps1
+++ bootstrap/windows/bootstrap.ps1
@@ -18,25 +18,22 @@ wsl --install -d Ubuntu
 winget install --source winget --id AltSnap.AltSnap
 winget install --source winget --id Discord.Discord
 winget install --source winget --id Docker.DockerDesktop
+winget install --source winget --id Giorgiotani.Peazip
+winget install --source winget --id Google.Chrome
+winget install --source winget --id Microsoft.PowerToys
 winget install --source winget --id Microsoft.WindowsTerminal.Preview
-winget install --source winget --id Valve.Steam
+winget install --source winget --id Mozilla.Firefox
+winget install --source winget --id Nextcloud.NextcloudDesktop
+winget install --source winget --id RandyRants.SharpKeys
+winget install --source winget --id Stardock.Start11
 winget install --source winget --id SumatraPDF.SumatraPDF
+winget install --source winget --id Valve.Steam
 winget install --source winget --id VideoLAN.VLC
 winget install --source winget --id WinSCP.WinSCP
-winget install --source winget --id RandyRants.SharpKeys
-winget install --source winget --id Giorgiotani.Peazip
-winget install --source winget --id Microsoft.PowerToys
-winget install --source winget --id Nextcloud.NextcloudDesktop
-winget install --source winget --id wez.wezterm
-winget install --source winget --id Google.Chrome
-winget install --source winget --id Mozilla.Firefox
-# winget install --source winget --id valinet.ExplorerPatcher
 winget install --source winget --id WireGuard.WireGuard
+winget install --source winget --id wez.wezterm
 
-# winget install --source msstore --id 9NBLGGH516XP # EarTrumpet
 winget install --source msstore 9NFNG39387K0 # Yubico Authenticator
-
-# winget install --source winget --id Postbox.Postbox
 
 # Scoop
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
