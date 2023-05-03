a12dcdcbc97b15e2e2950e5876b5488cfa36239e
Peter Tri Ho <mail@petertriho.com>
2023-05-04 09:45:43 +1000

    fix(nvim): override lspkind symbols

--- nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -199,7 +199,6 @@ return {
                         local cmp_format = require("lspkind").cmp_format({
                             mode = "symbol_text",
                             maxwidth = 80,
-                            symbol_map = { Codeium = "", Copilot = "" },
                             menu = {
                                 buffer = "[BUFFER]",
                                 cmdline = "[CMD]",
@@ -216,6 +215,37 @@ return {
                                 tmux = "[TMUX]",
                                 vsnip = "[SNIPPET]",
                                 ["vim-dadbod-completion"] = "[DB]",
+                            },
+                            symbol_map = {
+                                -- Defaults
+                                Text = "󰉿",
+                                Method = "󰆧",
+                                Function = "󰊕",
+                                Constructor = "",
+                                Field = "󰜢",
+                                Variable = "󰀫",
+                                Class = "󰠱",
+                                Interface = "",
+                                Module = "",
+                                Property = "󰜢",
+                                Unit = "󰑭",
+                                Value = "󰎠",
+                                Enum = "",
+                                Keyword = "󰌋",
+                                Snippet = "",
+                                Color = "󰏘",
+                                File = "󰈙",
+                                Reference = "󰈇",
+                                Folder = "󰉋",
+                                EnumMember = "",
+                                Constant = "󰏿",
+                                Struct = "󰙅",
+                                Event = "",
+                                Operator = "󰆕",
+                                TypeParameter = "",
+                                -- Custom
+                                Codeium = "",
+                                Copilot = "",
                             },
                         })
 
