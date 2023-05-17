4bbc561bcc38275ec96ab292a803f960d2f1588d
Peter Tri Ho <mail@petertriho.com>
2023-05-17 10:09:57 +1000

    refactor(nvim): simplify config for `trim.nvim`

--- nvim/.config/nvim/lua/plugins/config/trim.lua
+++ nvim/.config/nvim/lua/plugins/config/trim.lua
@@ -2,10 +2,9 @@ return {
     "cappyzawa/trim.nvim",
     config = function()
         require("trim").setup({
-            patterns = {
-                [[%s/\s\+$//e]],
-                [[%s/\($\n\s*\)\+\%$//]],
-            },
+            trim_trailing = true,
+            trim_first_line = false,
+            trim_last_line = true,
         })
     end,
     event = "BufWritePre",
