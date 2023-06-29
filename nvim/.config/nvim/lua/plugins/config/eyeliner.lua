39df9ab581233ede1df4d6485a4816ccd901a1a6
Peter Tri Ho <mail@petertriho.com>
2023-06-30 09:30:05 +1000

    revert(nvim): remove `eyeliner` hotfix

--- nvim/.config/nvim/lua/plugins/config/eyeliner.lua
+++ nvim/.config/nvim/lua/plugins/config/eyeliner.lua
@@ -1,28 +1,6 @@
 return {
     "jinh0/eyeliner.nvim",
     config = function()
-        local colors = require("colors")
-        local utils = require("eyeliner.utils")
-
-        local function enable_highlights()
-            vim.api.nvim_set_hl(0, "EyelinerPrimary", {
-                fg = colors.blue,
-                bold = true,
-                underline = true,
-            })
-            vim.api.nvim_set_hl(0, "EyelinerSecondary", {
-                fg = colors.red,
-                underline = true,
-            })
-            vim.api.nvim_set_hl(0, "EyelinerDimmed", { link = "Comment" })
-            return utils["set-autocmd"]("ColorScheme", {
-                callback = enable_highlights,
-                group = "Eyeliner",
-            })
-        end
-
-        require("eyeliner.shared")["enable-highlights"] = enable_highlights
-
         require("eyeliner").setup({
             highlight_on_key = false,
             match = "[a-z]",
