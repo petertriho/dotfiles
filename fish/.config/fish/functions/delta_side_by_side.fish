531f9c7bcd17b84af27c415379032d4ba51066b8
Peter Tri Ho <mail@petertriho.com>
2023-07-18 15:27:58 +1000

    fix(fish): export `COLUMNS` for `delta_side_by_side`

--- fish/.config/fish/functions/delta_side_by_side.fish
+++ fish/.config/fish/functions/delta_side_by_side.fish
@@ -1,4 +1,6 @@
 function delta_side_by_side --on-signal WINCH
+    set -x COLUMNS $COLUMNS
+
     if test "$COLUMNS" -ge 180; and ! contains side-by-side "$DELTA_FEATURES"
         set --global --export --append DELTA_FEATURES side-by-side
     else if test "$COLUMNS" -lt 180; and contains side-by-side "$DELTA_FEATURES"
