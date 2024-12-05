5c7cc593b4e9030d0e4f6f09fcf758a6153a2257
Peter Tri Ho <mail@petertriho.com>
2024-12-05 20:03:46 +1100

    feat(bootstrap): update installed apps

--- bootstrap/windows/bootstrap.ps1
+++ bootstrap/windows/bootstrap.ps1
@@ -17,15 +17,14 @@ winget install --source winget --id Ablaze.Floorp
 winget install --source winget --id AltSnap.AltSnap
 winget install --source winget --id Betterbird.Betterbird
 winget install --source winget --id Discord.Discord
-winget install --source winget --id Docker.DockerDesktop
 winget install --source winget --id Giorgiotani.Peazip
 winget install --source winget --id Google.Chrome
 winget install --source winget --id MartiCliment.UniGetUI
 winget install --source winget --id Microsoft.PowerToys
 winget install --source winget --id Mozilla.Firefox
+winget install --source winget --id Neovide.Neovide
 winget install --source winget --id Nextcloud.NextcloudDesktop
 winget install --source winget --id RandyRants.SharpKeys
-winget install --source winget --id RedHat.Podman-Desktop
 winget install --source winget --id SumatraPDF.SumatraPDF
 winget install --source winget --id Valve.Steam
 winget install --source winget --id VideoLAN.VLC
@@ -41,4 +40,3 @@ scoop install 7zip aria2 curl dark git neovim sudo win32yank
 scoop bucket add nerd-fonts
 scoop bucket add extras
 sudo scoop install --global JetBrainsMono-NF
-scoop install hwinfo wumgr
